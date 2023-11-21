7bee839af435318bfb1e3b734a887dcafa438faa
Artur Penar <penar.art@gmail.com>
2023-11-21 15:41:34 +0100

    Frontend: Add: ExerciseField consist exercises and set fields

---
+++ frontend/src/features/trainingLogs/components /ExerciseField.js
@@ -0,0 +1,61 @@
+import React from "react";
+import SetField from "./SetField";
+
+const ExerciseField = ({
+  exercises,
+  exerciseNameList,
+  handleExerciseChange,
+  handleSetsNumberChange,
+}) => {
+  return (
+    <div>
+      {exercises.map((exercise, index) => (
+        <div className="exercise" key={index}>
+          <div className="exercise-set-container">
+            <div className="exercise-left-panel">
+              <div className="form-group">
+                <label className="form-label" htmlFor={`exercise${index}`}>
+                  Name:
+                  <select
+                    className="form-control"
+                    name="exercise"
+                    value={exercise.exercise}
+                    onChange={(e) => handleExerciseChange(e, index)}
+                  >
+                    {exerciseNameList.map((name, i) => (
+                      <option key={i} value={name}>
+                        {name}
+                      </option>
+                    ))}
+                  </select>
+                  <div className="form-group">
+                    <label className="form-label" htmlFor={`sets${index}`}>
+                      Sets
+                      <input
+                        className="form-control"
+                        type="number"
+                        name="setsNumber"
+                        value={exercise.setsNumber}
+                        onChange={(e) => handleSetsNumberChange(e, index)}
+                      />
+                    </label>
+                  </div>
+                </label>
+              </div>
+            </div>
+
+            <SetField
+              exercise={exercise}
+              index={index}
+              setsNumber={exercise.setsNumber}
+              handleExerciseChange={handleExerciseChange}
+              handleSetsNumberChange={handleSetsNumberChange}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ExerciseField;
