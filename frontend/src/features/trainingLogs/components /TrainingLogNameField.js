abb764ef471a67354d59839bd7a6a7b5887d752e
Artur Penar <penar.art@gmail.com>
2023-11-21 16:41:59 +0100

    Frontend: Add: useEffect to TrainingLogNameField
    Added useEffect fucntion to set logName to first value from logNamesList.

--- frontend/src/features/trainingLogs/components /TrainingLogNameField.js
+++ frontend/src/features/trainingLogs/components /TrainingLogNameField.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const TrainingLogNameField = ({ logName, setLogName, logNames }) => {
-    return (
-        <div className="form-group">
-            <label className="form-label" htmlFor="name">
-                Name:
-                <select
-                    className="form-control"
-                    value={logName}
-                    onChange={(e) => setLogName(e.target.value)}
-                >
-                    {logNames.map((logNameItem, index) => (
-                        <option key={index} value={logNameItem}>
-                            {logNameItem}
-                        </option>
-                    ))}
-                </select>
-            </label>
-        </div>
-    );
+  useEffect(() => {
+    setLogName(logNames[0])
+  }, [logNames, setLogName]);
+  console.log(logName);
+
+  return (
+    <div className="form-group">
+      <label className="form-label" htmlFor="name">
+        Name:
+        <select
+          className="form-control"
+          value={logName}
+          onChange={(e) => {
+            setLogName(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          {logNames.map((logNameItem, index) => (
+            <option key={index} value={logNameItem}>
+              {logNameItem}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
 };
 
 export default TrainingLogNameField;
