3df6dff44080c797d5e136e372b8f4639ef30116
Artur Penar <penar.art@gmail.com>
2023-11-23 17:05:49 +0100

    Frontend: Add: Introduce css styling to AddTrainingSessionPage and associated components

--- frontend/src/features/trainingLogs/components /ExerciseField.js
+++ frontend/src/features/trainingLogs/components /ExerciseField.js
@@ -13,41 +13,41 @@ const ExerciseField = ({
         <div className="exercise" key={index}>
           <div className="exercise-set-container">
             <div className="exercise-left-panel">
+            <label className="form-label" htmlFor={`exercise${index}`}>
+              Exercise: {index + 1}
+            </label>
+            <div className="form-group">
               <label className="form-label" htmlFor={`exercise${index}`}>
-                Exercise: {index + 1}
-                <div className="form-group">
-                  <label className="form-label" htmlFor={`exercise${index}`}>
-                    Name:
-                    <select
-                      className="form-control"
-                      name="exercise"
-                      value={exercise.exercise}
-                      onChange={(e) => handleExerciseChange(e, index)}
-                    >
-                      {exerciseNameList.map((name, i) => (
-                        <option key={i} value={name}>
-                          {name}
-                        </option>
-                      ))}
-                    </select>
-                    <div className="form-group" >
-                      <label className="form-label" htmlFor={`sets${index}`}>
-                        Sets
-                        <input
-                          className="form-control"
-                          type="number"
-                          min="1"
-                          name="setsNumber"
-                          value={exercise.setsNumber || 1}
-                          onChange={(e) => handleSetsNumberChange(e, index)}
-                        />
-                      </label>
-                    </div>
-                  </label>
-                </div>
+                Name:
               </label>
+              <select
+                className="form-control"
+                name="exercise"
+                value={exercise.exercise}
+                onChange={(e) => handleExerciseChange(e, index)}
+              >
+                {exerciseNameList.map((name, i) => (
+                  <option key={i} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+              <div className="form-group">
+                <label className="form-label" htmlFor={`sets${index}`}>
+                  Sets
+                </label>
+                <input
+                  className="form-control"
+                  type="number"
+                  min="1"
+                  name="setsNumber"
+                  value={exercise.setsNumber || 1}
+                  onChange={(e) => handleSetsNumberChange(e, index)}
+                />
+              </div>
             </div>
-
+          </div>
+          <div className="exercise-right-panel">
             <SetField
               exercise={exercise}
               index={index}
@@ -57,6 +57,7 @@ const ExerciseField = ({
             />
           </div>
         </div>
+      </div>
       ))}
     </div>
   );
