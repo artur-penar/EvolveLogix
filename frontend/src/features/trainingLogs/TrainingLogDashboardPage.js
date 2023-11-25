82a588d0e66d10fc4c297349f9fd7a33ed561353
Artur Penar <penar.art@gmail.com>
2023-11-25 14:10:01 +0100

    Frontend: Update: TrainingLogDashboardPage rendering

--- frontend/src/features/trainingLogs/TrainingLogDashboardPage.js
+++ frontend/src/features/trainingLogs/TrainingLogDashboardPage.js
@@ -23,41 +23,51 @@ const TrainingLogDashboardPage = () => {
     navigate("/add-log", { state: { selectedDate: date.dateStr } });
   };
 
-  if (loading) {
-    console.log("Loading...");
-  } else {
-    console.log("Training logs");
-    console.log(trainingLogsData);
-  }
-
   return (
     <Layout title="PerformanceTracker| Training Log">
       <h1>Training Log Dashboard</h1>
-      <div style={{ width: "800px", height: "600px" }}>
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]} // include the interactionPlugin
-          initialView="dayGridMonth"
-          dateClick={handleDateClick}
-          firstDay={1}
-          events={[
-            { title: "[Done] Day A", date: "2023-10-30", color: "green" },
-            { title: "[Done] Day B", date: "2023-11-01", color: "green" },
-            { title: "[Done] Day C", date: "2023-11-03", color: "green" },
-            { title: "[Done] Day A", date: "2023-11-06", color: "green" },
-            { title: "[Done] Day B", date: "2023-11-07", color: "green" },
-            { title: "[Done] Day C", date: "2023-11-10", color: "green" },
-            { title: "[Miss] Day A", date: "2023-11-14", color: "grey" },
-            { title: "[Done] Day B", date: "2023-11-15", color: "green" },
-            { title: "[Done] Day C", date: "2023-11-17", color: "green" },
-            { title: "[Done] Day A", date: "2023-11-19", color: "green" },
-            { title: "[Done] Day B", date: "2023-11-21", color: "green" },
-            { title: "[Done] Day C", date: "2023-11-23", color: "green" },
-            { title: "[ToDo] Day A", date: "2023-11-27" },
-            { title: "[ToDo] Day B", date: "2023-11-29" },
-            { title: "[ToDo] Day C", date: "2023-12-01" },
-          ]}
-        />
-      </div>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            // justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div className="spinner-border" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <h2>Loading...</h2>
+        </div>
+      ) : (
+        <div style={{ width: "800px", height: "600px" }}>
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]} // include the interactionPlugin
+            initialView="dayGridMonth"
+            dateClick={handleDateClick}
+            firstDay={1}
+            events={
+              [
+                // { title: "[Done] Day A", date: "2023-10-30", color: "green" },
+                // { title: "[Done] Day B", date: "2023-11-01", color: "green" },
+                // { title: "[Done] Day C", date: "2023-11-03", color: "green" },
+                // { title: "[Done] Day A", date: "2023-11-06", color: "green" },
+                // { title: "[Done] Day B", date: "2023-11-07", color: "green" },
+                // { title: "[Done] Day C", date: "2023-11-10", color: "green" },
+                // { title: "[Miss] Day A", date: "2023-11-14", color: "grey" },
+                // { title: "[Done] Day B", date: "2023-11-15", color: "green" },
+                // { title: "[Done] Day C", date: "2023-11-17", color: "green" },
+                // { title: "[Done] Day A", date: "2023-11-19", color: "green" },
+                // { title: "[Done] Day B", date: "2023-11-21", color: "green" },
+                // { title: "[Done] Day C", date: "2023-11-23", color: "green" },
+                // { title: "[ToDo] Day A", date: "2023-11-27" },
+                // { title: "[ToDo] Day B", date: "2023-11-29" },
+                // { title: "[ToDo] Day C", date: "2023-12-01" },
+              ]
+            }
+          />
+        </div>
+      )}
     </Layout>
   );
 };
