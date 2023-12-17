1ef75b97c495647261b2098a05094e4018de6672
Artur Penar <penar.art@gmail.com>
2023-12-17 07:30:31 +0100

    Backend: Add: training session update path

--- backend/evolve_logix/training_log/urls.py
+++ backend/evolve_logix/training_log/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('exercises/', ExerciseListView.as_view()),
     path('', TrainingLogView.as_view()),
     path('sets-per-muscle-group/', SetsPerMuscleGroupView.as_view()),
-    path('training-session/<int:pk>/delete/', TrainingSessionUpdateDelete.as_view())
+    path('training-session/<int:pk>/delete/',
+         TrainingSessionUpdateDelete.as_view()),
+    path('training-session/<int:pk>/update/',
+         TrainingSessionUpdateDelete.as_view())
 ]
