5d55039ab67d1fcec0e8dee6dd110dd412d8d4b9
Artur Penar <penar.art@gmail.com>
2023-12-23 14:01:45 +0100

    Frontend: Update: AddTrainingSessionForm name to TrainingSessionForm
    Because it will be used in AddTrainingSession and EditTrainingsession pages.

---
+++ frontend/src/features/trainingLogs/components/TrainingSessionForm.js
@@ -0,0 +1,53 @@
+import React from "react";
+import TrainingLogNameField from "./TrainingLogNameField";
+import DateField from "./DateField";
+import ExerciseField from "./ExerciseField";
+import CommentField from "./CommentField";
+
+const TrainingSessionForm = ({
+  logName,
+  setLogName,
+  logNames,
+  date,
+  setDate,
+  comment,
+  setComment,
+  exercises,
+  exerciseNameList,
+  handleExerciseChange,
+  handleSetsNumberChange,
+  handleSubmit,
+  handleAddExercise,
+}) => (
+  <div className="add-training-container">
+    <div className="field-container">
+      <TrainingLogNameField
+        logName={logName}
+        setLogName={setLogName}
+        logNames={logNames}
+      />
+      <DateField date={date} setDate={setDate} />
+      <CommentField comment={comment} setComment={setComment} />
+    </div>
+    <form onSubmit={handleSubmit} className="form">
+      <ExerciseField
+        exercises={exercises}
+        exerciseNameList={exerciseNameList}
+        handleExerciseChange={handleExerciseChange}
+        handleSetsNumberChange={handleSetsNumberChange}
+      />
+      <button
+        type="button"
+        onClick={handleAddExercise}
+        className="button button-add-exercise"
+      >
+        Add Exercise
+      </button>
+      <button type="submit" className="button button-submit">
+        Submit
+      </button>
+    </form>
+  </div>
+);
+
+export default TrainingSessionForm;
