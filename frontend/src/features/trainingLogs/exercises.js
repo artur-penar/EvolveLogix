9f431171c7e1b86ab6f73be536e11ec92b411eb1
Artur Penar <penar.art@gmail.com>
2023-12-24 09:47:38 +0100

    Frontend: Add: exercises.js reducer
    And introduce them into store.js

---
+++ frontend/src/features/trainingLogs/exercises.js
@@ -0,0 +1,51 @@
+const { createAsyncThunk, createSlice } = require("@reduxjs/toolkit");
+
+export const getExercises = createAsyncThunk(
+  "exercises/getExercises",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("api/exercises", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const data = await res.json();
+      if (res.status === 200) {
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const initialState = {
+  exercises: [],
+  loading: false,
+  error: null,
+};
+
+const exercisesSlice = createSlice({
+  name: "exercises",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getExercises.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getExercises.fulfilled, (state, action) => {
+        state.loading = false;
+        state.exercises = action.payload;
+      })
+      .addCase(getExercises.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default exercisesSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "features/users/user";
 import logReducer from "features/trainingLogs/log";
+import exercisesReducer from "features/trainingLogs/exercises";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     log: logReducer,
+    exercises: exercisesReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
