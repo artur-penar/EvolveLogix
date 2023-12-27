40ead0ad979320a3a64d0ff0a51b2d8d95285590
Artur Penar <penar.art@gmail.com>
2023-12-27 12:13:20 +0100

    Frontend: Add: Fields to create training log
    And introduce them into DashboardPage, additionaly add styling in DashboardPage.css

--- frontend/src/features/users/DashboardPage.js
+++ frontend/src/features/users/DashboardPage.js
@@ -1,40 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "components/shared/Layout";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectIsUserAuthenticated, selectUser } from "features/users/user";
 import { Navigate } from "react-router-dom";
+import { getTrainingLogs } from "features/trainingLogs/log";
+import NewLogForm from "./components/NewLogForm";
+import LoadingState from "components/shared/LoadingState";
+import "./DashboardPage.css";
 
 const DashboardPage = () => {
+  // Redux state selectors
   const isAuthenticated = useSelector(selectIsUserAuthenticated);
   const user = useSelector(selectUser);
   const loading = useSelector((state) => state.user.loading);
+  const trainingLogs = useSelector((state) => state.log.trainingLogs);
+
+  // State variables
+  const [selectedLog, setSelectedLog] = useState("");
+  const [newLogName, setNewLogName] = useState("");
+
+  // Redux dispatch
+  const dispatch = useDispatch();
+
+  // Side effects
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(getTrainingLogs());
+      if (trainingLogs.length > 0) {
+        setSelectedLog(trainingLogs[0].name);
+      }
+    }
+  }, [dispatch]);
 
   if (!isAuthenticated && !loading) return <Navigate to="/login" />;
 
-  const { email, user_name } = user || {};
+  const handleChange = (e) => {
+    setSelectedLog(e.target.value);
+  };
 
-  console.log(email, user_name);
+  const handleSubmit = () => {
+    console.log("Submited");
+  };
 
   return (
     <Layout title="PerformanceTracker | Dashboard">
-      <h1>Dashboard</h1>
-      {loading || !user ? (
-        <div className="spinner-border" role="status">
-          <span className="sr-only"></span>
-        </div>
+      {loading || !user || !trainingLogs ? (
+        <LoadingState />
       ) : (
-        <React.Fragment>
-          <ul>
-            <li>
-              <strong>Email: </strong>
-              {email}
-            </li>
-            <li>
-              <strong>User Name: </strong>
-              {user_name}
-            </li>
-          </ul>
-        </React.Fragment>
+        <>
+          <h1>Current log:</h1>
+          <select onChange={handleChange} value={selectedLog}>
+            {trainingLogs.map((log, index) => (
+              <option key={index} value={log.name}>
+                {log.name}
+              </option>
+            ))}
+          </select>
+
+          <NewLogForm
+            newLogName={newLogName}
+            setNewLogName={setNewLogName}
+            handleSubmit={handleSubmit}
+          />
+        </>
       )}
     </Layout>
   );
