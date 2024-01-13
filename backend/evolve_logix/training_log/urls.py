9f26fdaa2800a814d0c41e31d02a0c72feef9695
Artur Penar <penar.art@gmail.com>
2024-01-13 14:44:39 +0100

    Backend: Add: TrainingSessionCreate path into urls.py

--- backend/evolve_logix/training_log/urls.py
+++ backend/evolve_logix/training_log/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ExerciseListView, TrainingLogView, SetsPerMuscleGroupView, TrainingSessionUpdateDelete
+from .views import ExerciseListView, TrainingLogView, SetsPerMuscleGroupView, TrainingSessionUpdateDelete, TrainingSessionCreate
 
 urlpatterns = [
     path('exercises/', ExerciseListView.as_view()),
     path('', TrainingLogView.as_view()),
     path('sets-per-muscle-group/', SetsPerMuscleGroupView.as_view()),
+    path('training-session/create/', TrainingSessionCreate.as_view()),
     path('training-session/<int:pk>/delete/',
          TrainingSessionUpdateDelete.as_view()),
     path('training-session/<int:pk>/update/',
