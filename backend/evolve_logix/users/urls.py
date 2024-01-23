b511c981557d2566bcdc7f124cbbb4317bc10741
Artur Penar <penar.art@gmail.com>
2024-01-23 16:07:54 +0100

    Backend: Add: Create detail path into urls.py

--- backend/evolve_logix/users/urls.py
+++ backend/evolve_logix/users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register', RegisterView.as_view()),
     path('me', RetrieveUserView.as_view()),
     path('list', ListUserView.as_view()),
-    path('user-detail', UserDetailCreateUpdateView.as_view()), 
+    path('detail/<int:pk>', UserDetailCreateUpdateView.as_view()),
 
 ]
