b2f352a9244230e307c3ff2741a4a5e0f85867ac
Artur Penar <penar.art@gmail.com>
2024-01-23 08:17:14 +0100

    Backed: Add: user-detail patch
    To retrieve UserDetail data.

--- backend/evolve_logix/users/urls.py
+++ backend/evolve_logix/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RegisterView, RetrieveUserView, ListUserView
+from .views import RegisterView, RetrieveUserView, ListUserView, UserDetailCreateUpdateView
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('me', RetrieveUserView.as_view()),
-    path('list', ListUserView.as_view())
+    path('list', ListUserView.as_view()),
+    path('user-detail', UserDetailCreateUpdateView.as_view()), 
 
 ]
