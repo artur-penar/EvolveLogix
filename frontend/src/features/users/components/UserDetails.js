8cbaf80a48feb57e1d2449cf3d7a1795c128a953
Artur Penar <penar.art@gmail.com>
2024-01-24 18:50:36 +0100

    Frontend: Add: Added dispatch - creteUserDetail

--- frontend/src/features/users/components/UserDetails.js
+++ frontend/src/features/users/components/UserDetails.js
@@ -2,21 +2,28 @@ import React, { useState } from "react";
 import DetailDisplay from "./DetailDisplay";
 import "./UserDetails.css";
 import DetailEditForm from "./DetailEditForm";
+import { createUserDetail, updateUserDetail } from "../user";
+import { useDispatch } from "react-redux";
 
 const UserDetails = ({ userDetail }) => {
-  const { updated_at, ...bodyMeasurements } = userDetail;
+  const { updated_at, ...bodyMeasurements } = userDetail[userDetail.length - 1];
   const updatedAtData = new Date(updated_at);
+
+  // React hooks
+  const dispatch = useDispatch();
 
   // State data
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState(bodyMeasurements);
-
   const handleEdit = () => {
     setIsEditing(true);
   };
 
   const handleSubmit = () => {
+    console.log("UserDetails: handleSubmit()");
+    console.log(formData);
     setIsEditing(false);
+    dispatch(createUserDetail(formData));
   };
 
   const handleInputChange = (event) => {
