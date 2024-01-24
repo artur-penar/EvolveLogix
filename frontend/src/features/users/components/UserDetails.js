1f2bb23a064cdbef9ce128da798bcaf873a7f5c2
Artur Penar <penar.art@gmail.com>
2024-01-24 08:37:29 +0100

    Frontend: Update: Separate updated_at in UserDetail
    And display them in more transparet way.

--- frontend/src/features/users/components/UserDetails.js
+++ frontend/src/features/users/components/UserDetails.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./UserDetails.css";
 
-const UserDetails = ({ userDetail }) => (
-  <div className="user-details-container">
-    <div className="body-measurements">
-      {Object.entries(userDetail).map(([key, value]) => (
-        <p key={key}>{`${key}: ${value}`}</p>
-      ))}
+const UserDetails = ({ userDetail }) => {
+  const { updated_at, ...bodyMeasurements } = userDetail;
+  const updatedAtData = new Date(updated_at);
+
+  return (
+    <div className="user-details-container">
+      <h3>User details:</h3>
+      <div className="body-measurements">
+        <p>{`Updated at: ${updatedAtData.toLocaleDateString()}`}</p>
+        {Object.entries(bodyMeasurements).map(([key, value]) => (
+          <p key={key}>{`${key}: ${value}`}</p>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UserDetails;
