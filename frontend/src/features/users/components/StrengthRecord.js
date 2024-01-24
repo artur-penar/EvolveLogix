63333b9a0ffed9c72dfe85883997f854c196c607
Artur Penar <penar.art@gmail.com>
2024-01-24 12:13:54 +0100

    Frontend: Update: StrengthRecord.js functionality

--- frontend/src/features/users/components/StrengthRecord.js
+++ frontend/src/features/users/components/StrengthRecord.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import DetailDisplay from "./DetailDisplay";
+import DetailEditForm from "./DetailEditForm";
 import "./StrengthRecords.css";
 
-const StrengthRecords = ({ userDetail }) => (
-  <div className="strength-records-container">
-    <h3>Strength records:</h3>
-    <div className="strength-records">
-      {Object.entries(userDetail).map(([key, value]) => (
-        <p key={key}>{`${key}: ${value}`}</p>
-      ))}
+const StrengthRecords = () => {
+  const userDetail = {
+    "bench press": 100,
+    deadlift: 200,
+    squat: 300,
+    "overhead press": 400,
+    "barbell row": 500,
+    updated_at: "2021-01-01",
+  };
+  const { updated_at, ...bodyMeasurements } = userDetail;
+  const updatedAtData = new Date(updated_at);
+
+  // State data
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState(bodyMeasurements);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  console.log(bodyMeasurements);
+
+  return (
+    <div className="user-details-container">
+      <h3>Strength Records:</h3>
+      <p>{`Updated at: ${updatedAtData.toLocaleDateString()}`}</p>
+      {isEditing ? (
+        <DetailEditForm
+          formData={formData}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+        />
+      ) : (
+        <DetailDisplay formData={formData} handleEdit={handleEdit} />
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default StrengthRecords;
