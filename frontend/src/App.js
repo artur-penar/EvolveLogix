18395a3e7dec0138483e5068d17433f98b47037a
Artur Penar <penar.art@gmail.com>
2024-01-24 16:23:18 +0100

    Frontend: Add: Introduce background picture in App.js
    But then code commented out.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,9 @@ import RegisterPage from "features/users/RegisterPage";
 import { verifyAuth } from "features/users/user";
 import EditTrainingSessionPage from "features/trainingLogs/pages/AddTrainingSession/EditTrainingSession";
 
+import backgroundImage from "./images/evolve_eagle.jpg";
+import secondBackgroundImage from "./images/evolve_background.jpg";
+
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -25,22 +28,34 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/exercises" element={<ExerciseListPage />} />
-        <Route path="/training-log" element={<TrainingLogDashboardPage />} />
-        <Route path="/view-logs" element={<ViewTrainingLogsPage />} />
-        <Route path="/add-log" element={<AddTrainingSessionPage />} />
-        <Route
-          path="/edit-training-session/:id"
-          element={<EditTrainingSessionPage />}
-        />
-      </Routes>
-    </Router>
+    <div
+      // style={{
+        // backgroundColor: "green", // This will set the background color to green
+        // backgroundImage: `url(${backgroundImage})`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundPosition: "center center",
+        // backgroundAttachment: "fixed",
+        // backgroundSize: "cover",
+        // height: "100vh",
+      // }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/exercises" element={<ExerciseListPage />} />
+          <Route path="/training-log" element={<TrainingLogDashboardPage />} />
+          <Route path="/view-logs" element={<ViewTrainingLogsPage />} />
+          <Route path="/add-log" element={<AddTrainingSessionPage />} />
+          <Route
+            path="/edit-training-session/:id"
+            element={<EditTrainingSessionPage />}
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
