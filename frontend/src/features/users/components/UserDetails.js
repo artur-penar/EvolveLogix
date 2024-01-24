eea1d01c029046cbd403532732405e0de8442191
Artur Penar <penar.art@gmail.com>
2024-01-24 10:56:43 +0100

    Frontend: Update: Introduce changes into UserDetail.js

--- frontend/src/features/users/components/UserDetails.js
+++ frontend/src/features/users/components/UserDetails.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import DetailDisplay from "./DetailDisplay";
 import "./UserDetails.css";
+import DetailEditForm from "./DetailEditForm";
 
 const UserDetails = ({ userDetail }) => {
   const { updated_at, ...bodyMeasurements } = userDetail;
   const updatedAtData = new Date(updated_at);
 
+  // State data
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState(bodyMeasurements);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div className="user-details-container">
       <h3>User details:</h3>
-      <div className="body-measurements">
-        <p>{`Updated at: ${updatedAtData.toLocaleDateString()}`}</p>
-        {Object.entries(bodyMeasurements).map(([key, value]) => (
-          <p key={key}>{`${key}: ${value}`}</p>
-        ))}
-      </div>
+      <p>{`Updated at: ${updatedAtData.toLocaleDateString()}`}</p>
+      {isEditing ? (
+        <DetailEditForm
+          formData={formData}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+        />
+      ) : (
+        <DetailDisplay formData={formData} handleEdit={handleEdit} />
+      )}
     </div>
   );
 };
