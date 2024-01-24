8e2978374d33936a5f125bca9e9326fe45c78150
Artur Penar <penar.art@gmail.com>
2024-01-24 08:14:04 +0100

    Frontend: Add: Create StrengthRecords.css
    And introduce them into StrengthRecords.js

--- frontend/src/features/users/components/StrengthRecord.js
+++ frontend/src/features/users/components/StrengthRecord.js
@@ -1,7 +1,9 @@
 import React from "react";
+import "./StrengthRecords.css";
 
 const StrengthRecords = ({ userDetail }) => (
-  <div className="log-details">
+  <div className="strength-records-container">
+    <h3>Strength records:</h3>
     <div className="strength-records">
       {Object.entries(userDetail).map(([key, value]) => (
         <p key={key}>{`${key}: ${value}`}</p>
