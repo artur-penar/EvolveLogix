da266b36f91f60cc5d538f136b0b6d7922de9ff3
Artur Penar <penar.art@gmail.com>
2024-03-28 16:22:13 +0100

    Frontend: Update styling for ViewTrainingLogsPage

--- frontend/src/features/trainingLogs/pages/ViewTrainingLogsPage.js
+++ frontend/src/features/trainingLogs/pages/ViewTrainingLogsPage.js
@@ -16,42 +16,47 @@ const ViewTrainingLogsPage = () => {
   console.log(trainingLogsData);
 
   return (
-    <Layout title="Auth Site | Training Log">
+    <Layout title="EvolveLogix | Training Log">
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <React.Fragment>
-          {trainingLogsData.map((log, index) => (
-            <div key={index}>
-              <>
-                <h1>{log.name}</h1>
-              </>
-              <div className="training-log-container">
-                {log.training_sessions.map((session, i) => (
-                  <div key={`${index}-{i}`} className="training-log">
-                    <h4>Date: {session.date}</h4>
-                    <h5>Comment: {session.comment}</h5>
-                    {session.exercises.map((exercise, j) => (
-                      <div key={j} className="ex">
-                        <p>
-                          Nr.{exercise.order} : {exercise.exercise}
-                        </p>
-                        {exercise.sets.map((set, k) => (
-                          <div key={k} className="set">
+        <>
+          <React.Fragment>
+            <div className="training-log-container">
+              {trainingLogsData.map((log, index) => (
+                <div key={index}>
+                  <div className="header-container">
+                    <h1>{log.name}</h1>
+                  </div>
+                  <div className="training-log-container">
+                    {log.training_sessions.map((session, i) => (
+                      <div key={`${index}-{i}`} className="training-log">
+                      <h4>Name: {session.description}</h4>
+                        <h5>Date: {session.date}</h5>
+                        <h5>Comment: {session.comment}</h5>
+                        {session.exercises.map((exercise, j) => (
+                          <div key={j} className="ex">
                             <p>
-                              Set: {set.set_number} {set.weight} x{" "}
-                              {set.repetitions}
+                              Nr.{exercise.order} : {exercise.exercise}
                             </p>
+                            {exercise.sets.map((set, k) => (
+                              <div key={k} className="set">
+                                <p>
+                                  Set {set.set_number}: {set.weight}kg x{" "}
+                                  {set.repetitions}rep
+                                </p>
+                              </div>
+                            ))}
                           </div>
                         ))}
                       </div>
                     ))}
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </React.Fragment>
+          </React.Fragment>
+        </>
       )}
     </Layout>
   );
