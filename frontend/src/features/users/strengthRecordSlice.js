2ea0f76602bd35701a2e1685dacbcb70fb01c1a1
Artur Penar <penar.art@gmail.com>
2024-04-06 18:08:34 +0200

    Frontend: Add createStrengthRecord action

--- frontend/src/features/users/strengthRecordSlice.js
+++ frontend/src/features/users/strengthRecordSlice.js
@@ -24,6 +24,32 @@ export const getAllStrengthRecords = createAsyncThunk(
   }
 );
 
+export const createStrengthRecord = createAsyncThunk(
+  "users/strength-records/create",
+  async (strengthRecord, thunkAPI) => {
+    const body = JSON.stringify(strengthRecord);
+    try {
+      const res = await fetch(`api/users/strength-records/create`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body,
+      });
+
+      const data = await res.json();
+      if (res.status === 201) {
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
 const initialState = {
   records: [],
   loading: false,
@@ -47,6 +73,16 @@ const strengthRecordSlice = createSlice({
         state.loading = false;
       })
       .addCase(getAllStrengthRecords.rejected, (state, action) => {
+        state.loading = false;
+      })
+      .addCase(createStrengthRecord.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createStrengthRecord.fulfilled, (state, action) => {
+        state.records.push(action.payload);
+        state.loading = false;
+      })
+      .addCase(createStrengthRecord.rejected, (state, action) => {
         state.loading = false;
       });
   },
