da890d1e257e7404b8fdf4cc33272106ac173e12
Artur Penar <penar.art@gmail.com>
2024-04-08 18:14:25 +0200

    Frontend: Add condition to prevent data loading error in StrengthRecords.js

--- frontend/src/features/users/pages/StrengthRecords/StrengthRecords.js
+++ frontend/src/features/users/pages/StrengthRecords/StrengthRecords.js
@@ -6,6 +6,7 @@ import Records from "features/users/components/StrengthRecords/Records";
 // frontend/src/features/users/components/StrengthRecords.js
 const StrengthRecords = () => {
   const strengthRecords = useSelector((state) => state.strengthRecords.records);
+  const isLoading = useSelector((state) => state.strengthRecords.loading);
 
   console.log("Strength Records page");
   console.log(strengthRecords);
@@ -16,7 +17,14 @@ const StrengthRecords = () => {
         <div className="records-header-container">
           <h1>Strength Records</h1>
         </div>
-        <Records strengthRecords={strengthRecords} styleClassName={"strength-records-container"}></Records>
+        {isLoading ? (
+          <p>Loading</p>
+        ) : (
+          <Records
+            strengthRecords={strengthRecords}
+            styleClassName={"strength-records-container"}
+          ></Records>
+        )}
       </div>
     </Layout>
   );
