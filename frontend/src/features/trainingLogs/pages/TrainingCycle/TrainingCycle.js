fa4581712b8f8f20f82faf5fafce40787196ce3c
Artur Penar <penar.art@gmail.com>
2024-04-10 16:42:13 +0200

    Frontend: Add cycles selection in TrainingCycle.js

--- frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycle.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycle.js
@@ -7,14 +7,54 @@ const TrainingCycle = () => {
   const phases = ["Hypertrophy", "Strength", "Peaking", "Deload"];
   const [phase, setPhase] = useState(phases[0]);
 
+  const mesocycles = ["Competition prep", "Off-season", "Transition"];
+  const [mesocycle, setMesocycle] = useState(mesocycles[0]);
+
+  const macrocycles = ["2024Cycle", "Quadrennial"];
+  const [macrocycle, setMacrocycle] = useState(macrocycles[0]);
+
   const handlePhaseChange = (e) => {
     setPhase(e.target.value);
+  };
+
+  const handleMesocycleChange = (e) => {
+    setMesocycle(e.target.value);
+  };
+
+  const handleMacrocycleChange = (e) => {
+    setMacrocycle(e.target.value);
   };
 
   return (
     <Layout title="EvolveLogix | Training cycle">
       <div className="training-cycle-content">
         <PageHeader headerContent={"Training Cycle"} />
+        <label>Macrocycle:</label>
+        <select
+          className="form-control"
+          value={macrocycle}
+          onChange={handleMacrocycleChange}
+          style={{ width: "60%" }}
+        >
+          {macrocycles.map((name, i) => (
+            <option key={i} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <label>Mesocycle:</label>
+        <select
+          className="form-control"
+          value={mesocycle}
+          onChange={handleMesocycleChange}
+          style={{ width: "60%" }}
+        >
+          {mesocycles.map((name, i) => (
+            <option key={i} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
         <label>Phase</label>
         <select
           className="form-control"
