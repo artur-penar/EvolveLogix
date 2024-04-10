1a56e4b547e31d76782f06b125d53252790cbf00
Artur Penar <penar.art@gmail.com>
2024-04-10 16:28:27 +0200

    Frontend: Add phase selection into TrainingCycle.js

--- frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycle.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycle.js
@@ -1,14 +1,33 @@
 import Layout from "components/shared/Layout";
 import PageHeader from "components/shared/PageHeader";
-import React from "react";
+import React, { useState } from "react";
 import "./TrainingCycle.css";
 
 const TrainingCycle = () => {
+  const phases = ["Hypertrophy", "Strength", "Peaking", "Deload"];
+  const [phase, setPhase] = useState(phases[0]);
+
+  const handlePhaseChange = (e) => {
+    setPhase(e.target.value);
+  };
+
   return (
     <Layout title="EvolveLogix | Training cycle">
       <div className="training-cycle-content">
         <PageHeader headerContent={"Training Cycle"} />
-        <label>Macro cycle:</label>
+        <label>Phase</label>
+        <select
+          className="form-control"
+          value={phase}
+          onChange={handlePhaseChange}
+          style={{ width: "60%" }}
+        >
+          {phases.map((name, i) => (
+            <option key={i} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
       </div>
     </Layout>
   );
