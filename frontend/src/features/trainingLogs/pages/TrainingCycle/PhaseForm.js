50ea677109c814c3dc5f883b266375a00f09d091
Artur Penar <penar.art@gmail.com>
2024-04-18 15:25:07 +0200

    Frontend: Add weight select field based on percent of 1RM in PhaseForm.js

--- frontend/src/features/trainingLogs/pages/TrainingCycle/PhaseForm.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/PhaseForm.js
@@ -1,40 +1,70 @@
 import React from "react";
 import "./PhaseForm.css";
 
-const PhaseForm = ( {weeks}) => {
+const PhaseForm = ({ weeks }) => {
   // const weeks = 4;
   const fields = ["Squat", "Bench", "Deadlift", "Overhead press"];
+  const exercises = 3;
+  const trainingDays = 3;
+  console.log(exercises);
+
   return (
     <div className="phase-form-container">
       <h4 className="header-container">Phase programming</h4>
-      <div className="phase-week-container">
-        <div className="week">
-          <label>Day 1:</label>
-        </div>
-        {Array.from({ length: weeks }, (_, i) => i + 1).map((week) => (
-          <div key={week} className="week">
-            <label>Week {week}</label>
-          </div>
-        ))}
-      </div>
-      <div className="phase-week-container">
-        <div className="week">
-          <select className="exercise-select">
-            {fields.map((field) => (
-              <option value={field}>{field}</option>
+      {Array.from({ length: trainingDays }, (_, i) => i + 1).map((day) => (
+        <div key={day}>
+          <div className="phase-week-container">
+            <div className="week">
+              <label>Day {day}:</label>
+            </div>
+            {Array.from({ length: weeks }, (_, i) => i + 1).map((week) => (
+              <div key={week} className="week">
+                <label>Week {week}</label>
+              </div>
             ))}
-          </select>
-        </div>
-        {Array.from({ length: weeks }, (_, i) => i + 1).map((week) => (
-          <div key={week} className="week">
-            <input className="phase-input" type="text" placeholder="weight" />
-            <label>x</label>
-            <input className="phase-input" type="text" placeholder="reps" />
-            <label>x</label>
-            <input className="phase-input" type="text" placeholder="sets" />
           </div>
-        ))}
-      </div>
+          {Array.from({ length: exercises }, (_, i) => i + 1).map(
+            (exercise) => (
+              <div className="phase-week-container">
+                <div className="week">
+                  <select className="exercise-select">
+                    {fields.map((field) => (
+                      <option key={field} value={field}>
+                        {field}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                {Array.from({ length: weeks }, (_, i) => i + 1).map((week) => (
+                  <div key={week} className="week">
+                    <select className="phase-input">
+                      {Array.from({ length: 100 }, (_, i) => i + 1).map(
+                        (weight) => (
+                          <option key={weight} value={weight}>
+                            {weight}%
+                          </option>
+                        )
+                      )}
+                    </select>
+                    <label>x</label>
+                    <input
+                      className="phase-input"
+                      type="text"
+                      placeholder="reps"
+                    />
+                    <label>x</label>
+                    <input
+                      className="phase-input"
+                      type="text"
+                      placeholder="sets"
+                    />
+                  </div>
+                ))}
+              </div>
+            )
+          )}
+        </div>
+      ))}
     </div>
   );
 };
