73dd692b8841b420e1da60873c71d00f5e8e8d17
Artur Penar <penar.art@gmail.com>
2024-04-18 18:35:42 +0200

    Frontend: Update PhaseForm.css class names
    And introduce them into PhaseForm.js

--- frontend/src/features/trainingLogs/pages/TrainingCycle/PhaseForm.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/PhaseForm.js
@@ -6,21 +6,21 @@ const PhaseForm = ({ weekNumber, trainingDays }) => {
   const exercisesNameList = ["Squat", "Bench", "Deadlift", "Overhead press"];
   const exercisesNumber = 3;
 
-  const renderWeekLabels = (weeks) =>
+  const renderWeekLabels = (weeksNumber) =>
     Array.from(
       {
-        length: parseInt(weeks, 10) + 1,
+        length: parseInt(weeksNumber, 10) + 1,
       },
       (_, i) => i + 1
     ).map((week) => (
-      <div key={week} className="week">
+      <div key={week} className="week-label">
         <label>Week {week}</label>
       </div>
     ));
 
   const renderSelectExerciseField = (exercisesNameList) => {
     return (
-      <div className="week">
+      <div className="exercise-select-container">
         <select className="exercise-select">
           {exercisesNameList.map((exerciseName) => (
             <option key={exerciseName} value={exerciseName}>
@@ -33,7 +33,7 @@ const PhaseForm = ({ weekNumber, trainingDays }) => {
   };
 
   const renderWeightSelect = () => (
-    <select className="phase-input">
+    <select className="input">
       {Array.from({ length: 100 }, (_, i) => i + 1).map((weightInPercent) => (
         <option key={weightInPercent} value={weightInPercent}>
           {weightInPercent}%
@@ -48,38 +48,41 @@ const PhaseForm = ({ weekNumber, trainingDays }) => {
     exercisesNameList
   ) =>
     Array.from({ length: exercisesNumber }, (_, i) => i + 1).map((exercise) => (
-      <div key={exercise} className="phase-week-container">
+      <div key={exercise} className="week-container">
         {renderSelectExerciseField(exercisesNameList)}
         {Array.from(
           { length: parseInt(weekNumber, 10) + 1 },
           (_, i) => i + 1
         ).map((week) => (
-          <div key={week} className="week">
+          <div key={week} className="exercise-inputs-container">
             {renderWeightSelect()}
             <label>x</label>
-            <input className="phase-input" type="text" placeholder="reps" />
+            <input className="input" type="text" placeholder="reps" />
             <label>x</label>
-            <input className="phase-input" type="text" placeholder="sets" />
+            <input className="input" type="text" placeholder="sets" />
           </div>
         ))}
       </div>
     ));
 
   return (
-    <div className="phase-form-container">
+    <div className="form-container">
       <h4 className="header-container">Phase programming</h4>
       {Array.from(
         { length: parseInt(trainingDays, 10) + 1 },
         (_, i) => i + 1
       ).map((trainingDay) => (
-        <div key={trainingDay}>
-          <div className="phase-week-container">
-            <div className="week">
+        <div className="training-day-container" key={trainingDay}>
+          <div className="week-container">
+            <div className="week-label">
               <label>Day {trainingDay}:</label>
             </div>
             {renderWeekLabels(weekNumber)}
           </div>
           {renderWeekComponents(exercisesNumber, weekNumber, exercisesNameList)}
+          <div className="button-container">
+            <button>Add exercise</button>
+          </div>
         </div>
       ))}
     </div>
