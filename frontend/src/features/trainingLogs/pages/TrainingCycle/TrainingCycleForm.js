00dcac05c98f96ba60da72d88e501dfa0b49c82b
Artur Penar <penar.art@gmail.com>
2024-04-25 19:34:46 +0200

    Frontend: Update TrainingCycleForm.js components setup

--- frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycleForm.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/TrainingCycleForm.js
@@ -33,23 +33,7 @@ const TrainingCycleForm = ({
             ))}
           </select>
         </div>
-        <div className="tcf-select-group">
-          <label className="tcf-select-label">Phase:</label>
-          <select
-            className="form-control tcf-select-control"
-            value={phase}
-            onChange={handlePhaseChange}
-          >
-            {phases.map((name, i) => (
-              <option key={i} value={name}>
-                {name}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
 
-      <div className="tcf-select-container">
         <div className="tcf-select-group">
           <label className="tcf-select-label">Mesocycle:</label>
           <select
@@ -64,7 +48,23 @@ const TrainingCycleForm = ({
             ))}
           </select>
         </div>
+      </div>
 
+      <div className="tcf-select-container">
+        <div className="tcf-select-group">
+          <label className="tcf-select-label">Phase:</label>
+          <select
+            className="form-control tcf-select-control"
+            value={phase}
+            onChange={handlePhaseChange}
+          >
+            {phases.map((name, i) => (
+              <option key={i} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className="tcf-select-group">
           <label className="tcf-select-label">Duration in weeks:</label>
           <select
