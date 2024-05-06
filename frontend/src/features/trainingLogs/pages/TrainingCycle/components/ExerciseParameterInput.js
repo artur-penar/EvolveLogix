08f193beb171ca0d28beeb2d8119f118b88d4d24
Artur Penar <penar.art@gmail.com>
2024-05-06 17:50:13 +0200

    Frontend: Update ExerciseParameterInput.js
    - Change default input value from 0 to null to display 'weight', 'reps', 'sets' placeholders to increase understanding of input fields destination.

--- frontend/src/features/trainingLogs/pages/TrainingCycle/components/ExerciseParameterInput.js
+++ frontend/src/features/trainingLogs/pages/TrainingCycle/components/ExerciseParameterInput.js
@@ -37,7 +37,7 @@ const ExerciseParameterInput = ({
       <input
         className="input"
         type="text"
-        value={value || 0}
+        value={value || null}
         onChange={(e) =>
           handleChange(
             trainingDayIndex,
