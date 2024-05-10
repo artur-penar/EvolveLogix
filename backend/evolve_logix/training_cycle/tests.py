d31a25648dcd3abdf92f6fcde60975157388e78e
Artur Penar <penar.art@gmail.com>
2024-05-10 18:42:48 +0200

    Backend: Add tests for Mesocycle model

--- backend/evolve_logix/training_cycle/tests.py
+++ backend/evolve_logix/training_cycle/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Mesocycle
+from datetime import date
 
 # Create your tests here.
+
+
+class MesocycleModelTest(TestCase):
+    def setUp(self):
+        self.mesocycle = Mesocycle.objects.create(
+            name='Test Mesocycle', start_date=date.today(), end_date=date.today())
