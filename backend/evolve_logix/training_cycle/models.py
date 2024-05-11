604a17e5f52f32b62a9f65777207658677fad787
Artur Penar <penar.art@gmail.com>
2024-05-11 15:29:33 +0200

    Backend: Add Phase model into 'training_cycle' app
    - Create migrations for this model.
    - Create tests.

--- backend/evolve_logix/training_cycle/models.py
+++ backend/evolve_logix/training_cycle/models.py
@@ -17,3 +17,16 @@ class Macrocycle(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Phase(models.Model):
+    macrocycle = models.ForeignKey(Macrocycle, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- backend/evolve_logix/training_cycle/tests.py
+++ backend/evolve_logix/training_cycle/tests.py
@@ -1,16 +1,28 @@
 from django.test import TestCase
-from .models import Mesocycle
+from .models import Mesocycle, Macrocycle
 from datetime import date
 
 # Create your tests here.
 
 
-class MesocycleModelTest(TestCase):
-    def setUp(self):
-        self.mesocycle = Mesocycle.objects.create(
-            name='Test Mesocycle', start_date=date.today())
+# class MesocycleModelTest(TestCase):
+#     def setUp(self):
+#         self.mesocycle = Mesocycle.objects.create(
+#             name='Test Mesocycle', start_date=date.today())
 
-    def test_mesocycle_creation(self):
-        self.assertEqual(self.mesocycle.name, 'Test Mesocycle')
-        self.assertEqual(self.mesocycle.start_date, date.today())
-        self.assertEqual(self.mesocycle.end_date, None)
+#     def test_mesocycle_creation(self):
+#         self.assertEqual(self.mesocycle.name, 'Test Mesocycle')
+#         self.assertEqual(self.mesocycle.start_date, date.today())
+#         self.assertEqual(self.mesocycle.end_date, None)
+
+
+class MacrocycleModelTest(TestCase):
+    def setUp(self):
+        self.mesocycle = Mesocycle.objects.create(name='Mesocycle')
+        self.macrocycle = Macrocycle.objects.create(
+            mesocycle=self.mesocycle, name='Macrocycle')
+
+    def test_macrocycle_creation(self):
+        print(self.macrocycle.__dict__)
+        self.assertEqual(self.macrocycle.name, 'Macrocycle')
+        self.assertEqual(self.macrocycle.mesocycle, self.mesocycle)
