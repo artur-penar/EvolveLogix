44abeb8d32c102f6d2abd97191e85fd2dde85f65
Artur Penar <penar.art@gmail.com>
2024-05-12 11:34:36 +0200

    Backend: Add MesocycleListCreateview

--- backend/evolve_logix/training_cycle/views.py
+++ backend/evolve_logix/training_cycle/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Mesocycle
+from .serializers import MesocycleSerializer
 
 # Create your views here.
+
+
+class MesocycleListCreateView(generics.ListCreateAPIView):
+    serializer_class = MesocycleSerializer
+
+    def get_queryset(self):
+        return Mesocycle.objects.filter(user=self.request.user)
