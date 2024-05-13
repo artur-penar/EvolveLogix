9e7093a9a3639375411c38bbc6f33e5083b319a5
Artur Penar <penar.art@gmail.com>
2024-05-13 17:25:49 +0200

    Backend: Update tests_views.py
    - Fix 'test_create_mesocycle'.

--- backend/evolve_logix/training_cycle/tests_views.py
+++ backend/evolve_logix/training_cycle/tests_views.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
 from .models import Mesocycle
-from users.models import UserProfile
 from django.contrib.auth import get_user_model
 
 
@@ -20,14 +18,17 @@ class MesocycleListCreateViewTest(TestCase):
             email='otheruser@example.com', user_name='Other User', password='otherpass')
         Mesocycle.objects.create(name='Mesocycle 2', user=other_user)
 
-    def test_list_mesocycles(self):
+    def obtain_login_token(self):
         # Obtain a JWT for the test user
         response = self.client.post(reverse('token_obtain_pair'), data={
             'email': 'testuser@example.com',
             'password': 'testpass',
         })
         self.assertEqual(response.status_code, 200)
-        token = response.data['access']
+        return response.data['access']
+
+    def test_list_mesocycles(self):
+        token = self.obtain_login_token()
 
         # Send a GET request to the MesocycleListCreateView with the JWT in the Authorization header
         response = self.client.get(
@@ -39,4 +40,55 @@ class MesocycleListCreateViewTest(TestCase):
         # Check that the response contains the Mesocycle object that belongs to the user
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], 'Mesocycle 1')
-        print("Response data: ", response.data)
+
+    def test_create_mesocycle(self):
+        token = self.obtain_login_token()
+        print("user id", self.user.id)
+
+        # Send a POST request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.post(reverse('mesocycle-list-create'), data={
+            'name': 'Mesocycle 3'},
+            HTTP_AUTHORIZATION=f'Bearer {token}',
+            content_type='application/json'
+        )
+
+        # Check that the response has a status code of 201 (Created)
+        self.assertEqual(response.status_code, 201)
+        print("Response ", response.data)
+
+    def test_integration_mesocycle_list_create_view(self):
+        # Obtain a JWT for the test user
+        token = self.obtain_login_token()
+
+        # Send a GET request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.get(
+            reverse('mesocycle-list-create'), HTTP_AUTHORIZATION=f'Bearer {token}')
+
+        # Check that the response has a status code of 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the response contains the Mesocycle object that belongs to the user
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'Mesocycle 1')
+
+        # Send a POST request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.post(reverse('mesocycle-list-create'), data={
+            'name': 'Mesocycle 3'},
+            HTTP_AUTHORIZATION=f'Bearer {token}',
+            content_type='application/json'
+        )
+
+        # Check that the response has a status code of 201 (Created)
+        self.assertEqual(response.status_code, 201)
+
+        # Send another GET request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.get(
+            reverse('mesocycle-list-create'), HTTP_AUTHORIZATION=f'Bearer {token}')
+
+        # Check that the response has a status code of 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the response contains the two Mesocycle objects that belong to the user
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['name'], 'Mesocycle 1')
+        self.assertEqual(response.data[1]['name'], 'Mesocycle 3')
