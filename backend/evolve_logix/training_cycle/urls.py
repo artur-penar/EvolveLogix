9a484c33a40002fd7510a10cd81dc3cc4ace2952
Artur Penar <penar.art@gmail.com>
2024-05-13 16:01:00 +0200

    Backend: Update urls.py in 'training_cycle' app
    - Update MesocycleListCreateView import.

--- backend/evolve_logix/training_cycle/urls.py
+++ backend/evolve_logix/training_cycle/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import MesocycleListCreateView
+from .views import MesocycleListCreateView
 
 urlpatterns = [
     path('mesocycles/', MesocycleListCreateView.as_view(),
