bbe14be97f451fc97427c5614c2573ffbc16450f
Artur Penar <penar.art@gmail.com>
2024-05-22 17:20:04 +0200

    Backend: Update 'training_cycle' app admin.py file
    - Add 'training_cycle' models to admin.py file.

--- backend/evolve_logix/training_cycle/admin.py
+++ backend/evolve_logix/training_cycle/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from training_cycle import models
 
 # Register your models here.
+admin.site.register(models.Mesocycle)
+admin.site.register(models.Macrocycle)
+admin.site.register(models.Phase)
+admin.site.register(models.Microcycle)
+admin.site.register(models.TrainingSession)
+admin.site.register(models.ExerciseInSession)
