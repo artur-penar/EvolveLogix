2c1990afc5f9fc7a37d791a1c1a493e84981c307
Artur Penar <penar.art@gmail.com>
2024-05-26 10:32:18 +0200

    Backend: Update 'training_cycle' serizlisers.py file
    - Added nested serizliers to Macrocycle, Mesocycle, Phase, TrainingSession, ExerciseInSession serizlizers, to enable sending nested data via api.

--- backend/evolve_logix/training_cycle/serializers.py
+++ backend/evolve_logix/training_cycle/serializers.py
@@ -2,37 +2,51 @@ from rest_framework import serializers
 from .models import Macrocycle, Mesocycle, Phase, Microcycle, TrainingSession, ExerciseInSession
 
 
-class MacrocycleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Macrocycle
-        fields = ['id', 'user', 'name', 'start_date', 'end_date']
-
-
-class MesocycleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mesocycle
-        fields = ['id', 'macrocycle', 'name', 'start_date', 'end_date']
-
-
-class PhaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Phase
-        fields = ['id', 'mesocycle', 'type', 'start_date', 'end_date']
-
-
 class MicrocycleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Microcycle
         fields = '__all__'
 
 
-class TrainingSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TrainingSession
-        fields = '__all__'
-
-
 class ExerciseInSessionSerializer(serializers.ModelSerializer):
+    microcycle_set = MicrocycleSerializer(many=True, read_only=True)
+
     class Meta:
         model = ExerciseInSession
-        fields = '__all__'
+        fields = ['id', 'training_session', 'exercise', 'microcycle_set']
+
+
+class TrainingSessionSerializer(serializers.ModelSerializer):
+    exercises = ExerciseInSessionSerializer(
+        many=True, read_only=True)
+
+    class Meta:
+        model = TrainingSession
+        fields = ['id', 'phase', 'order', 'exercises']
+
+
+class PhaseSerializer(serializers.ModelSerializer):
+    trainingsession_set = TrainingSessionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Phase
+        fields = ['id', 'mesocycle', 'type', 'start_date',
+                  'end_date', 'trainingsession_set']
+
+
+class MesocycleSerializer(serializers.ModelSerializer):
+    phase_set = PhaseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Mesocycle
+        fields = ['id', 'macrocycle', 'name',
+                  'start_date', 'end_date', 'phase_set']
+
+
+class MacrocycleSerializer(serializers.ModelSerializer):
+    mesocycle_set = MesocycleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Macrocycle
+        fields = ['id', 'user',
+                  'name', 'start_date', 'end_date', 'mesocycle_set']
