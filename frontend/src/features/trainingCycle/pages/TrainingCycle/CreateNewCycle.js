199e09708d9fe5f1cbfee9671ecd6d2addc7f53d
Artur Penar <penar.art@gmail.com>
2024-05-30 19:48:53 +0200

    Frontend: Update CreateNewCycle.js
    - Added variables to store cycle type and name.
    - Added function to handle change cycle typa and name.

--- frontend/src/features/trainingCycle/pages/TrainingCycle/CreateNewCycle.js
+++ frontend/src/features/trainingCycle/pages/TrainingCycle/CreateNewCycle.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateNewCycle.css";
 
 const CreateNewCycle = () => {
+  const [cycleName, setCycleName] = useState("");
+  const [cycleType, setCycleType] = useState("Macrocycle");
+
+  const handleNameChange = (e) => {
+    setCycleName(e.target.value);
+  };
+
+  const handleTypeChange = (e) => {
+    setCycleType(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(cycleType, cycleName);
+  };
   return (
     <div className="cnc-container">
       <h4 className="cnc-header-container">Create new cycle</h4>
       <div className="cnc-form-container">
         <div className="cnc-form-group">
           <label for="cycle">Cycle:</label>
-          <select id="cycle" className="form-control">
+          <select
+            id="cycle"
+            className="form-control"
+            value={cycleType}
+            onChange={handleTypeChange}
+          >
             {["Macrocycle", "Mesocycle"].map((name, i) => (
               <option key={i} value={name}>
                 {name}
@@ -19,10 +38,18 @@ const CreateNewCycle = () => {
 
         <div className="cnc-form-group">
           <label for="name">Name:</label>
-          <input id="name" type="text" className="form-control"></input>
+          <input
+            id="name"
+            type="text"
+            className="form-control"
+            value={cycleName}
+            onChange={handleNameChange}
+          ></input>
         </div>
       </div>
-      <button className="submit-button">Submit</button>
+      <button className="submit-button" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 };
