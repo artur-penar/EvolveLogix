b3f188c47910991e049cd9b52958057b0e50a453
Artur Penar <penar.art@gmail.com>
2024-06-02 09:08:33 +0200

    Backend: Update tests_views.py

--- backend/evolve_logix/training_cycle/tests_views.py
+++ backend/evolve_logix/training_cycle/tests_views.py
@@ -58,25 +58,24 @@ class BaseTest(TestCase):
         return response.data['access']
 
 
-# class MacrocycleListCreateViewTest(BaseTest):
-#     def test_list_macrocycles(self):
-#         # Send a GET request to the MacrocycleListCreateView with the JWT in the Authorization header
-#         response = self.client.get(reverse('macrocycle-list-create'),
-#                                    HTTP_AUTHORIZATION=f'Bearer {self.token}')
-#         print("Macrocycle list response")
-#         self.assertEqual(response.status_code, 200)
+class MacrocycleListCreateViewTest(BaseTest):
+    def test_list_macrocycles(self):
+        # Send a GET request to the MacrocycleListCreateView with the JWT in the Authorization header
+        response = self.client.get(reverse('macrocycle-list-create'),
+                                   HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        print("Macrocycle list response")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_create_macrocycle(self):
-#         # Send a POST request to the MacrocycleListCreateView with the JWT in the Authorization header
-#         response = self.client.post(reverse('macrocycle-list-create'), data={
-#             'user': self.user.pk,
-#             'name': 'Macrocycle 3',
-#             'start_date': datetime.date.today(),
-#         },
-#             HTTP_AUTHORIZATION=f'Bearer {self.token}',
-#             content_type='application/json')
+    def test_create_macrocycle(self):
+        # Send a POST request to the MacrocycleListCreateView with the JWT in the Authorization header
+        response = self.client.post(reverse('macrocycle-list-create'), data={
+            'user': self.user.pk,
+            'name': 'Macrocycle 3',
+        },
+            HTTP_AUTHORIZATION=f'Bearer {self.token}',
+            content_type='application/json')
 
-#         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 201)
 
 
 # class MacrocycleRetrieveUpdateDestroyViewTest(BaseTest):
@@ -108,31 +107,31 @@ class BaseTest(TestCase):
 #         self.assertEqual(Mesocycle.objects.count(), 1)
 
 
-# class MesocycleListCreateViewTest(BaseTest):
-#     def test_list_mesocycles(self):
-#         # Send a GET request to the MesocycleListCreateView with the JWT in the Authorization header
-#         response = self.client.get(
-#             reverse('mesocycle-list-create'), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+class MesocycleListCreateViewTest(BaseTest):
+    def test_list_mesocycles(self):
+        # Send a GET request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.get(
+            reverse('mesocycle-list-create'), HTTP_AUTHORIZATION=f'Bearer {self.token}')
 
-#         # Check that the response has a status code of 200 (OK)
-#         self.assertEqual(response.status_code, 200)
+        # Check that the response has a status code of 200 (OK)
+        self.assertEqual(response.status_code, 200)
 
-#         # Check that the response contains the Mesocycle object that belongs to the user
-#         self.assertEqual(len(response.data), 2)
-#         self.assertEqual(response.data[0]['name'], 'Mesocycle 1')
+        # Check that the response contains the Mesocycle object that belongs to the user
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['name'], 'Mesocycle 1')
 
-#     def test_create_mesocycle(self):
-#         # Send a POST request to the MesocycleListCreateView with the JWT in the Authorization header
-#         response = self.client.post(reverse('mesocycle-list-create'), data={
-#             'name': 'Mesocycle 3',
-#             'macrocycle': self.macrocycle1.pk,
-#         },
-#             HTTP_AUTHORIZATION=f'Bearer {self.token}',
-#             content_type='application/json'
-#         )
+    def test_create_mesocycle(self):
+        # Send a POST request to the MesocycleListCreateView with the JWT in the Authorization header
+        response = self.client.post(reverse('mesocycle-list-create'), data={
+            'name': 'Mesocycle 3',
+            'macrocycle': self.macrocycle1.pk,
+        },
+            HTTP_AUTHORIZATION=f'Bearer {self.token}',
+            content_type='application/json'
+        )
 
-#         # Check that the response has a status code of 201 (Created)
-#         self.assertEqual(response.status_code, 201)
+        # Check that the response has a status code of 201 (Created)
+        self.assertEqual(response.status_code, 201)
 
 #     def test_integration_mesocycle_list_create_view(self):
 #         # Send a GET request to the MesocycleListCreateView with the JWT in the Authorization header
@@ -224,33 +223,33 @@ class BaseTest(TestCase):
 #         self.assertEqual(Phase.objects.count(), 3)
 
 
-class PhaseRetrieveUpdateDestroyViewTest(BaseTest):
-    def test_retrieve_phase(self):
-        # Send a GET request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
-        response = self.client.get(
-            reverse('phase-detail', kwargs={'pk': self.phase.pk}), HTTP_AUTHORIZATION=f'Bearer {self.token}')
-        self.assertEqual(response.status_code, 200)
+# class PhaseRetrieveUpdateDestroyViewTest(BaseTest):
+#     def test_retrieve_phase(self):
+#         # Send a GET request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
+#         response = self.client.get(
+#             reverse('phase-detail', kwargs={'pk': self.phase.pk}), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+#         self.assertEqual(response.status_code, 200)
 
-    def test_update_phase(self):
-        # Send a PUT request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
-        response = self.client.put(
-            reverse('phase-detail', kwargs={'pk': self.phase.pk}),
-            data={'type': 'Deload', 'mesocycle': self.mesocycle1.pk,
-                  'start_date': datetime.date.today()},
-            HTTP_AUTHORIZATION=f'Bearer {self.token}',
-            content_type='application/json'
-        )
-        print(response.data)
-        self.assertEqual(response.status_code, 200)
+#     def test_update_phase(self):
+#         # Send a PUT request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
+#         response = self.client.put(
+#             reverse('phase-detail', kwargs={'pk': self.phase.pk}),
+#             data={'type': 'Deload', 'mesocycle': self.mesocycle1.pk,
+#                   'start_date': datetime.date.today()},
+#             HTTP_AUTHORIZATION=f'Bearer {self.token}',
+#             content_type='application/json'
+#         )
+#         print(response.data)
+#         self.assertEqual(response.status_code, 200)
 
-    def test_delete_phase(self):
-        # Send a DELETE request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
-        response = self.client.delete(
-            reverse('phase-detail', kwargs={'pk': self.phase.pk}),
-            HTTP_AUTHORIZATION=f'Bearer {self.token}'
-        )
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(Phase.objects.count(), 1)
+#     def test_delete_phase(self):
+#         # Send a DELETE request to the PhaseRetrieveUpdateDestroyView with the JWT in the Authorization header
+#         response = self.client.delete(
+#             reverse('phase-detail', kwargs={'pk': self.phase.pk}),
+#             HTTP_AUTHORIZATION=f'Bearer {self.token}'
+#         )
+#         self.assertEqual(response.status_code, 204)
+#         self.assertEqual(Phase.objects.count(), 1)
 
 
 # class MicrocycleListCreateViewTest(BaseTest):
