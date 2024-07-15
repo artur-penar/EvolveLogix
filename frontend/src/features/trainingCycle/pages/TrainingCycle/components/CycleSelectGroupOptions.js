e0cb61fde5e16dcc63f4853c71a4c36458aba3cd
Artur Penar <penar.art@gmail.com>
2024-07-15 18:34:45 +0200

    Fronted: Add CycleSelectGroupOptions.js file
    - Created reusable component to move 'Options' selectiong logic from CycleSelectGroup's.

---
+++ frontend/src/features/trainingCycle/pages/TrainingCycle/components/CycleSelectGroupOptions.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const CycleSelectGroupOptions = ({ options }) => {
+  return (
+    <div
+      style={{
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+        marginBottom: "1rem",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          borderBottom: "1px solid rgb(0, 0, 0, 0.1",
+          padding: "0.5rem",
+        }}
+      >
+        <h4>Options</h4>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          padding: "1rem",
+        }}
+      >
+        {options.map((option) => {
+          return (
+            <label>
+              <input
+                type="checkbox"
+                checked={option.checked}
+                onChange={(e) => option.onChange(e.target.checked)}
+                style={{ marginRight: "0.5rem" }}
+              />
+              {option.label}
+            </label>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CycleSelectGroupOptions;
