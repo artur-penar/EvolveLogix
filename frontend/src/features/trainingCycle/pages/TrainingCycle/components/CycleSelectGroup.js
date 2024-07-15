264ced105937b019c7080cf96e5dac710d1ca8f8
Artur Penar <penar.art@gmail.com>
2024-07-15 16:43:42 +0200

    Frontend: Add CycleSelectGroup.js file
    - Created reusable component responsible for rendering groups of select option used in TrainingCycleForm.

---
+++ frontend/src/features/trainingCycle/pages/TrainingCycle/components/CycleSelectGroup.js
@@ -0,0 +1,65 @@
+import React from "react";
+import SelectInput from "./SelectInput";
+import DateInput from "./DateInput";
+
+const CycleSelectGroup = ({
+  type,
+  value,
+  options,
+  handleInputChange,
+  startDate,
+  endDate,
+  additionalProps,
+}) => {
+  const name = type.toLowerCase();
+  return (
+    <div className="tcf-select-group-container">
+      <h3>{type}</h3>
+      <div className="tcf-select-container">
+        <div className="flex-column">
+          <SelectInput
+            name={name}
+            label="Name"
+            value={value}
+            options={options}
+            handleChange={handleInputChange}
+          />
+          {type === "Phase" && (
+            <SelectInput
+              name="trainingDays"
+              label="Training days"
+              value={additionalProps.trainingDays}
+              options={[...Array(7).keys()].map((n) => n + 1)}
+              handleChange={handleInputChange}
+            />
+          )}
+          {type === "Mesocycle" && (
+            <SelectInput
+              name="mesocycleDurationInWeeks"
+              label="Duration in weeks"
+              value={additionalProps.mesocycleDurationInWeeks}
+              options={[...Array(11).keys()]}
+              handleChange={handleInputChange}
+              disabled={true}
+            />
+          )}
+        </div>
+        <div className="flex-column">
+          {type === "Phase" && (
+            <SelectInput
+              name="phaseDurationInWeeks"
+              label="Duration in weeks"
+              value={additionalProps.phaseDurationInWeeks}
+              options={[...Array(10).keys()].map((n) => n + 1)}
+              handleChange={handleInputChange}
+            />
+          )}
+          <DateInput label="Start date:" value={startDate} />
+          <DateInput label="End date:" value={endDate} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CycleSelectGroup;
