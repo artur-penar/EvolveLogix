4e8e371e6b4a48b71d374976578f7643a59677cc
Artur Penar <penar.art@gmail.com>
2024-07-21 09:47:20 +0200

    Frontend: Update PhaseDisplay.js
    - Added logic handling phase selection and displaying.

--- frontend/src/features/trainingCycle/pages/TrainingCycle/components/PhaseDisplay.js
+++ frontend/src/features/trainingCycle/pages/TrainingCycle/components/PhaseDisplay.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TrainingSessionContainer from "./TrainingSessionContainer";
 import "./PhaseDisplay.css";
 
+const processPhaseData = (data) => {
+  if (!data) return [];
+  const trainingSessions = data.training_sessions;
+  return trainingSessions.map(({ order, exercises }) => ({
+    dayNumber: order,
+    exercises: exercises.map(({ exercise, microcycles }) => ({
+      exercise,
+      microcycles, // Include microcycles here
+    })),
+  }));
+};
+
 const PhaseDisplay = ({ phasesData }) => {
-  const processPhaseData = (data) => {
-    if (!data) return [];
-    const trainingSessions = data.training_sessions;
-    return trainingSessions.map(({ order, exercises }) => ({
-      dayNumber: order,
-      exercises: exercises.map(({ exercise, microcycles }) => ({
-        exercise,
-        microcycles, // Include microcycles here
-      })),
-    }));
-  };
+  const [selectedPhaseId, setSelectedPhaseId] = useState("");
+  const [processedSelectedPhaseData, setProcessedSelectedPhaseData] = useState(
+    []
+  );
+  const phaseSelectionOptions = phasesData.map((phase) => ({
+    id: phase.id,
+    type: phase.type,
+  }));
 
-  const processedData = processPhaseData(phasesData[phasesData.length - 1]);
+  useEffect(() => {
+    if (phaseSelectionOptions.length && selectedPhaseId === "")
+      setSelectedPhaseId(phaseSelectionOptions[0].id);
+  }, [phasesData]);
 
-  if (!processedData.length) {
+  useEffect(() => {
+    const selectedPhaseData = phasesData.find(
+      (phase) => phase.id == selectedPhaseId
+    );
+    setProcessedSelectedPhaseData(processPhaseData(selectedPhaseData));
+  }, [selectedPhaseId]);
+
+  if (!processedSelectedPhaseData.length) {
     return null;
   }
 
@@ -25,17 +44,23 @@ const PhaseDisplay = ({ phasesData }) => {
     <div className="phase-display-container">
       <h3>Phase Display</h3>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <label>Phase name: {phasesData[0].phase}</label>
-        <select>
-          <option value="mesocycle">Mesocycle</option>
-          <option value="macrocycle">Macrocycle</option>
+        <label>Phase name: </label>
+        <select
+          value={selectedPhaseId}
+          onChange={(e) => setSelectedPhaseId(e.target.value)}
+        >
+          {phaseSelectionOptions.map((phase) => (
+            <option key={phase.id} value={phase.id}>
+              {phase.type}
+            </option>
+          ))}
         </select>
       </div>
-      {processedData.map((_, trainingSessionIndex) => (
+      {processedSelectedPhaseData.map((_, trainingSessionIndex) => (
         <TrainingSessionContainer
           trainingSessionIndex={trainingSessionIndex}
-          totalMicrocyclesNumber={3}
-          phaseTrainingProgram={processedData}
+          totalMicrocyclesNumber={processedSelectedPhaseData.length}
+          phaseTrainingProgram={processedSelectedPhaseData}
           exercisesNameList={["Squat", "Bench press", "Deadlift"]}
           handleExerciseChange={() => {}}
           handleExerciseDetailChange={() => {}}
