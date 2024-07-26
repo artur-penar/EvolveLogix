915c939b264d85919dc16af710e5a479cf650b1d
Artur Penar <penar.art@gmail.com>
2024-07-26 16:51:45 +0200

    Frontend: Update ViewTrainingCycle.js
    - Added Phase detail render functionality.

--- frontend/src/features/trainingCycle/pages/ViewTrainingCycle/ViewTrainingCycles.js
+++ frontend/src/features/trainingCycle/pages/ViewTrainingCycle/ViewTrainingCycles.js
@@ -1,11 +1,53 @@
 import Layout from "components/shared/Layout";
 import React from "react";
+import { useSelector } from "react-redux";
+import PhaseDisplay from "../TrainingCycle/components/PhaseDisplay";
+import CycleTimeline from "../TrainingCycle/CycleTimeline";
+
+// What i want to do here is to display the selected macrocycle and its mesocycles and phases
+// I need to consider the way of displaying the data
+// One possibility is to use PhaseDisplay component to display the phases
+// It could be a good idea to use timeline to display the mesocycles chronology and duration
 
 const ViewTrainingCycles = () => {
+  const trainingCyclesState = useSelector((state) => state.trainingCycle);
+  const selectedMacrocycle = useSelector(
+    (state) => state.trainingCycle.selectedMacrocycle
+  );
+  const selectedMacrocycleData = trainingCyclesState.trainingCycles.find(
+    (macrocycle) => macrocycle.name === selectedMacrocycle
+  );
+  const selectedMacrocycleMesocycles = selectedMacrocycleData?.mesocycles || [];
+  const firstPhase = selectedMacrocycleMesocycles[0]?.phases[0] || [];
+  const phasesData = [];
+  phasesData.push(firstPhase);
+
   console.log("ViewTrainingCycles.js");
+  console.log(trainingCyclesState);
+  console.log(selectedMacrocycle);
+  console.log(selectedMacrocycleData);
+  console.log(selectedMacrocycleMesocycles);
+  console.log(firstPhase);
+  console.log(firstPhase.length);
+
+  const Mesocycle = ({ mesocycle }) => (
+    <div key={mesocycle.id}>
+      <h3>{mesocycle.name}</h3>
+      {mesocycle.phases.map((phase) => (
+        <PhaseDisplay
+          key={phase.id}
+          phasesData={[phase]}
+          enableSelect={false}
+        />
+      ))}
+    </div>
+  );
   return (
     <Layout title="Evolve Logix | View Training Cycles">
       <h1>View Training Cycles</h1>
+      {selectedMacrocycleMesocycles.map((mesocycle) => (
+        <Mesocycle key={mesocycle.id} mesocycle={mesocycle} />
+      ))}
     </Layout>
   );
 };
