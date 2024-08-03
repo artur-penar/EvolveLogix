3b74ca18d46158e35f4acbda9e9dff9785bdbb33
Artur Penar <penar.art@gmail.com>
2024-08-03 07:52:09 +0200

    Frotend: Refractor useHandleExerciseChange custom hook
    - Provided docstring describing hook destination.

--- frontend/src/features/trainingCycle/hooks/handlers/useHandleExerciseChange.js
+++ frontend/src/features/trainingCycle/hooks/handlers/useHandleExerciseChange.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+/**
+ * Custom hook to handle changing the name of an exercise in a training session.
+ *
+ * @param {Function} setPhaseTrainingProgram - Function to update the phase training program state.
+ * @returns {Function} handleExerciseChange - Function to change the name of an exercise in a specified training session.
+ *
+ * The handleExerciseChange function takes three arguments:
+ * - trainingSessionIndex: Index of the training session containing the exercise to be changed.
+ * - exerciseIndex: Index of the exercise to be changed within the specified training session.
+ * - newExerciseName: New name for the exercise.
+ *
+ * The function creates a deep copy of the current phase training program state,
+ * updates the name of the specified exercise, and then updates the phase training program state with the new state.
+ */
+
 const useHandleExerciseChange = (setPhaseTrainingProgram) => {
   const handleExerciseChange = (
     trainingSessionIndex,
