f56b284c64ec2f1010732f58ddcb147f5de187d9
Artur Penar <penar.art@gmail.com>
2024-08-30 18:11:49 +0200

    Frontend: Update useAutoClearStatus custom hook
    - Replaced 'setTimeout' function by 'setInterval'.
    - Added 'countdown' variable which are returned to parent component.
    - After interval finish, status has cleared.

--- frontend/src/features/trainingCycle/hooks/PhaseForm/useAutoClearStatus.js
+++ frontend/src/features/trainingCycle/hooks/PhaseForm/useAutoClearStatus.js
@@ -1,21 +1,34 @@
-const { useEffect } = require("react");
+const { useEffect, useState } = require("react");
 
 /**
  * Custom hook to automatically clear the status after a specified timeout.
  *
  * @param {string|null} addRequestStatus - The current status of the add request.
  * @param {Function} setAddRequestStatus - Function to set the status of the add request.
+ * @returns {number} - The countdown value in seconds.
  */
-
 const useAutoClearStatus = (addRequestStatus, setAddRequestStatus) => {
+  const [countdown, setCountdown] = useState(10);
+
   useEffect(() => {
+    let interval;
     if (addRequestStatus) {
-      setTimeout(() => {
-        setAddRequestStatus(null);
-      }, 10000);
+      setCountdown(10); // Reset countdown to 10 seconds
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => {
+          if (prevCountdown <= 1) {
+            clearInterval(interval);
+            setAddRequestStatus(null);
+            return 0;
+          }
+          return prevCountdown - 1;
+        });
+      }, 1000);
     }
-    return () => clearTimeout();
-  }, [addRequestStatus]);
+    return () => clearInterval(interval);
+  }, [addRequestStatus, setAddRequestStatus]);
+
+  return countdown;
 };
 
 export default useAutoClearStatus;
