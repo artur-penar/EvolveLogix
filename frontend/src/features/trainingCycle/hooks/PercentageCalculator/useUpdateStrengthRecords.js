ae6357d922ad059b02a77c86a51b23f1cb3db2b3
Artur Penar <penar.art@gmail.com>
2024-09-19 20:05:48 +0200

    Frontend: Update useUpdateStrengthRecords
    - Added docstring.

--- frontend/src/features/trainingCycle/hooks/PercentageCalculator/useUpdateStrengthRecords.js
+++ frontend/src/features/trainingCycle/hooks/PercentageCalculator/useUpdateStrengthRecords.js
@@ -1,5 +1,12 @@
 import { useMemo } from "react";
 
+/**
+ * This hook updates the strength records to include an "Other" exercise.
+ * This allows the user to input their own weight and calculate the converted value.
+ * @param {Array} strengthRecords - The strength records to update
+ * @returns {Array} - The updated strength records
+ */
+
 const useUpdateStrengthRecords = (strengthRecords) => {
   return useMemo(() => {
     return strengthRecords.length === 0
