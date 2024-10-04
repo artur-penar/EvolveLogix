cd6d4b22084fe7f9cc773d769963d2caadea7e67
Artur Penar <penar.art@gmail.com>
2024-10-04 17:17:32 +0200

    Frontend: Update TrainingSessionHeader component
    - Provided setTrainingSessionDate prop.
    - Added names to input fields.

--- frontend/src/features/trainingLogs/pages/AddTrainingSession/components/TrainingSessionHeader.js
+++ frontend/src/features/trainingLogs/pages/AddTrainingSession/components/TrainingSessionHeader.js
@@ -3,6 +3,7 @@ import React from "react";
 const TrainingSessionHeader = ({
   description,
   trainingSessionDate,
+  setTrainingSessionDate,
   comment,
   setComment,
 }) => {
@@ -10,13 +11,14 @@ const TrainingSessionHeader = ({
     <div className="ats-training-session-header">
       <h4>Name: {description}</h4>
       <label>Date:</label>
-      <input type="date" value={trainingSessionDate} />
-      <label>Comment:</label>
       <input
-        type="text"
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
+        name="date"
+        type="date"
+        value={trainingSessionDate}
+        onChange={setTrainingSessionDate}
       />
+      <label>Comment:</label>
+      <input name="comment" type="text" value={comment} onChange={setComment} />
     </div>
   );
 };
