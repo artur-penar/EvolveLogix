27337be628b7e7ae585f7326751b3aeef11c0001
Artur Penar <penar.art@gmail.com>
2024-10-06 10:38:22 +0200

    Frontend: Update TrainingSession component
    - Fetched strength record of selected exercise and passed on to ExerciseParameterInput.

--- frontend/src/features/trainingCycle/pages/TrainingCycle/components/Shared/TrainingSession/TrainingSession.js
+++ frontend/src/features/trainingCycle/pages/TrainingCycle/components/Shared/TrainingSession/TrainingSession.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SelectExerciseField from "./SelectExerciseField";
 import ExerciseParameterInput from "./ExerciseParameterInput";
+import { getLatestStrengthRecords } from "features/trainingCycle/utils/getLatestStrengthRecords";
 
 const TrainingSession = ({
   phaseTrainingProgram,
@@ -12,72 +13,82 @@ const TrainingSession = ({
   handleExerciseDetailChange,
   displayWeightInPercent,
   isEditable,
+  strengthRecords,
 }) => {
   if (!phaseTrainingProgram[trainingDayIndex]) {
     return null;
   }
 
+  const trainingDayExercises =
+    phaseTrainingProgram[trainingDayIndex]["exercises"];
+  const strengthRecord = getLatestStrengthRecords(strengthRecords);
+
   return Array.from({ length: exercisesNumber }, (_, i) => i).map(
-    (exerciseIndex) => (
-      <div key={exerciseIndex} className="week-container">
-        <SelectExerciseField
-          phaseTrainingProgram={phaseTrainingProgram}
-          exerciseNamesList={exerciseNamesList}
-          exerciseIndex={exerciseIndex}
-          dayIndex={trainingDayIndex}
-          handleExerciseChange={handleExerciseChange}
-          isEditable={isEditable}
-        />
-        {Array.from({ length: totalMicrocyclesNumber }, (_, i) => i).map(
-          (microcycleIndex) => (
-            <div key={microcycleIndex} className="exercise-inputs-container">
-              <ExerciseParameterInput
-                trainingDayIndex={trainingDayIndex}
-                exerciseIndex={exerciseIndex}
-                weekIndex={microcycleIndex}
-                value={
-                  phaseTrainingProgram[trainingDayIndex]?.exercises[
-                    exerciseIndex
-                  ]?.microcycles[microcycleIndex]?.weight
-                }
-                handleChange={handleExerciseDetailChange}
-                detailType="weight"
-                displayWeightInPercent={displayWeightInPercent}
-                isEditable={isEditable}
-              />
-              <label>x</label>
-              <ExerciseParameterInput
-                trainingDayIndex={trainingDayIndex}
-                exerciseIndex={exerciseIndex}
-                weekIndex={microcycleIndex}
-                value={
-                  phaseTrainingProgram[trainingDayIndex]?.exercises[
-                    exerciseIndex
-                  ]?.microcycles[microcycleIndex]?.repetitions
-                }
-                handleChange={handleExerciseDetailChange}
-                detailType="repetitions"
-                isEditable={isEditable}
-              />
-              <label>x</label>
-              <ExerciseParameterInput
-                trainingDayIndex={trainingDayIndex}
-                exerciseIndex={exerciseIndex}
-                weekIndex={microcycleIndex}
-                value={
-                  phaseTrainingProgram[trainingDayIndex]?.exercises[
-                    exerciseIndex
-                  ]?.microcycles[microcycleIndex]?.sets
-                }
-                handleChange={handleExerciseDetailChange}
-                detailType="sets"
-                isEditable={isEditable}
-              />
-            </div>
-          )
-        )}
-      </div>
-    )
+    (exerciseIndex) => {
+      const exerciseName = trainingDayExercises[exerciseIndex]["exercise"];
+      const record = strengthRecord[exerciseName]?.[0].weight;
+      return (
+        <div key={exerciseIndex} className="week-container">
+          <SelectExerciseField
+            phaseTrainingProgram={phaseTrainingProgram}
+            exerciseNamesList={exerciseNamesList}
+            exerciseIndex={exerciseIndex}
+            dayIndex={trainingDayIndex}
+            handleExerciseChange={handleExerciseChange}
+            isEditable={isEditable}
+          />
+          {Array.from({ length: totalMicrocyclesNumber }, (_, i) => i).map(
+            (microcycleIndex) => (
+              <div key={microcycleIndex} className="exercise-inputs-container">
+                <ExerciseParameterInput
+                  trainingDayIndex={trainingDayIndex}
+                  exerciseIndex={exerciseIndex}
+                  weekIndex={microcycleIndex}
+                  value={
+                    phaseTrainingProgram[trainingDayIndex]?.exercises[
+                      exerciseIndex
+                    ]?.microcycles[microcycleIndex]?.weight
+                  }
+                  handleChange={handleExerciseDetailChange}
+                  detailType="weight"
+                  displayWeightInPercent={displayWeightInPercent}
+                  isEditable={isEditable}
+                  strengthRecord={record}
+                />
+                <label>x</label>
+                <ExerciseParameterInput
+                  trainingDayIndex={trainingDayIndex}
+                  exerciseIndex={exerciseIndex}
+                  weekIndex={microcycleIndex}
+                  value={
+                    phaseTrainingProgram[trainingDayIndex]?.exercises[
+                      exerciseIndex
+                    ]?.microcycles[microcycleIndex]?.repetitions
+                  }
+                  handleChange={handleExerciseDetailChange}
+                  detailType="repetitions"
+                  isEditable={isEditable}
+                />
+                <label>x</label>
+                <ExerciseParameterInput
+                  trainingDayIndex={trainingDayIndex}
+                  exerciseIndex={exerciseIndex}
+                  weekIndex={microcycleIndex}
+                  value={
+                    phaseTrainingProgram[trainingDayIndex]?.exercises[
+                      exerciseIndex
+                    ]?.microcycles[microcycleIndex]?.sets
+                  }
+                  handleChange={handleExerciseDetailChange}
+                  detailType="sets"
+                  isEditable={isEditable}
+                />
+              </div>
+            )
+          )}
+        </div>
+      );
+    }
   );
 };
 
