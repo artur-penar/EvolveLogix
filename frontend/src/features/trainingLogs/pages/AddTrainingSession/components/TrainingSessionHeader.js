3984c6857a088c26055ea5f654d9d00c338d1dc6
Artur Penar <penar.art@gmail.com>
2024-10-07 19:05:55 +0200

    Frontend: Update TrainingSessionHeader component
    - Added normaliztaion 'Description' field, which now contains values from 'Training A' to 'Training G'.

--- frontend/src/features/trainingLogs/pages/AddTrainingSession/components/TrainingSessionHeader.js
+++ frontend/src/features/trainingLogs/pages/AddTrainingSession/components/TrainingSessionHeader.js
@@ -3,19 +3,38 @@ import React from "react";
 const TrainingSessionHeader = ({
   description,
   trainingSessionDate,
-  setTrainingSessionDate,
+  handleTrainingDataChange,
   comment,
   setComment,
 }) => {
+  const startCharCode = "A".charCodeAt(0);
+  const endCharCode = "G".charCodeAt(0);
+  const trainingOptions = [];
+
+  for (let i = startCharCode; i <= endCharCode; i++) {
+    trainingOptions.push(`Training ${String.fromCharCode(i)}`);
+  }
+
   return (
     <div className="ats-training-session-header">
-      <h4>Name: {description}</h4>
+      <label>Training tag</label>
+      <select
+        name="description"
+        value={description}
+        onChange={handleTrainingDataChange}
+      >
+        {trainingOptions.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
       <label>Date:</label>
       <input
         name="date"
         type="date"
         value={trainingSessionDate}
-        onChange={setTrainingSessionDate}
+        onChange={handleTrainingDataChange}
       />
       <label>Comment:</label>
       <input name="comment" type="text" value={comment} onChange={setComment} />
