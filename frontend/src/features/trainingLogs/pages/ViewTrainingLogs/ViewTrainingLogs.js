030c073d11a21a1dfe9dfb7124f13ca4a1a4c521
Artur Penar <penar.art@gmail.com>
2024-10-15 08:53:02 +0200

    Frontend: Update ViewTrainingLogs.js
    - Added 'sortedTrainingSessions' variable.
    - Render training sessions based on 'sortedTrainingSessions'.
    - Added 'trainingDaysPerWeek' varaible defining colum number in row.
    - Added inline styling in 'training-session-container' to set the colum number in row.

--- frontend/src/features/trainingLogs/pages/ViewTrainingLogs/ViewTrainingLogs.js
+++ frontend/src/features/trainingLogs/pages/ViewTrainingLogs/ViewTrainingLogs.js
@@ -11,14 +11,15 @@ const ViewTrainingLogsPage = () => {
     dispatch(getTrainingLogs());
   }, []);
 
-  const trainingLogsData = useSelector((state) => state.log.trainingLogs);
-  console.log("Training logs: ", trainingLogsData);
   // Fetch current selected training log
   const selectedTrainingLog = useSelector(
     (state) => state.log.selectedTrainingLog
   );
-  console.log("Selected training log: ", selectedTrainingLog);
   const trainingSessions = selectedTrainingLog.training_sessions;
+  const sortedTrainingSessions = [...trainingSessions].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+  console.log("Sorted training sessions: ", sortedTrainingSessions);
   const loading = useSelector((state) => state.log.loading);
 
   const groupTrainingSessions = (trainingSessions) => {
@@ -54,21 +55,28 @@ const ViewTrainingLogsPage = () => {
   };
 
   const groupedTrainingSessions = groupTrainingSessions(trainingSessions);
-  console.log(calculateTrainingDaysPerWeek(groupedTrainingSessions));
+  const trainingDaysPerWeek = calculateTrainingDaysPerWeek(
+    groupedTrainingSessions
+  );
 
   return (
     <Layout title="EvolveLogix | Training Log">
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div className="training-log-container">
+        <div>
           <div className="header-container">
             <h1>{selectedTrainingLog.name}</h1>
           </div>
-          <div className="training-log-container">
-            {trainingSessions.map((session, i) => (
-              <div key={i} className="training-log">
-                <h4>Name: {session.description}</h4>
+          <div
+            className="training-sessions-container"
+            style={{
+              gridTemplateColumns: ` repeat(${trainingDaysPerWeek}, minmax(300px, 1fr)  )`,
+            }}
+          >
+            {sortedTrainingSessions.map((session, i) => (
+              <div key={i} className="training-session">
+                <h4>Tag: {session.description}</h4>
                 <h5>Date: {session.date}</h5>
                 <h5>Comment: {session.comment}</h5>
                 {session.exercises.map((exercise, j) => (
