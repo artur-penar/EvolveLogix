5a8cf9e56918bf9c6a89e10d97194615de17465f
Artur Penar <penar.art@gmail.com>
2024-10-16 09:38:41 +0200

    Frontend: Remove unnecessary code from ViewTrainingLogs page

--- frontend/src/features/trainingLogs/pages/ViewTrainingLogs/ViewTrainingLogs.js
+++ frontend/src/features/trainingLogs/pages/ViewTrainingLogs/ViewTrainingLogs.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getTrainingLogs } from "../../log";
+import GroupedTrainingSessions from "./components/GroupedTrainingSessions";
 import Layout from "components/shared/Layout";
 import "./ViewTrainingLogs.css";
-import { getWeek } from "date-fns";
-import GroupedTrainingSessions from "./components/GroupedTrainingSessions";
 
 const ViewTrainingLogsPage = () => {
   const dispatch = useDispatch();
@@ -17,48 +16,7 @@ const ViewTrainingLogsPage = () => {
     (state) => state.log.selectedTrainingLog
   );
   const trainingSessions = selectedTrainingLog.training_sessions;
-  const sortedTrainingSessions = [...trainingSessions].sort(
-    (a, b) => new Date(a.date) - new Date(b.date)
-  );
-  console.log("Sorted training sessions: ", sortedTrainingSessions);
   const loading = useSelector((state) => state.log.loading);
-
-  const groupTrainingSessions = (trainingSessions) => {
-    const trainingSessionGroups = {
-      "Training A": [],
-      "Training B": [],
-      "Training C": [],
-    };
-    trainingSessions.map((session) => {
-      const sessionDate = new Date(session.date);
-      const weekNumber = getWeek(sessionDate, { weekStartsOn: 1 });
-
-      if (trainingSessionGroups[session.description]) {
-        trainingSessionGroups[session.description].push({
-          ...session,
-          weekNumber,
-        });
-      }
-    });
-
-    console.log(trainingSessionGroups);
-    return trainingSessionGroups;
-  };
-
-  const calculateTrainingDaysPerWeek = (groupedTrainingSessions) => {
-    let trainingDaysPerWeek = 0;
-    Object.keys(groupedTrainingSessions).map((key) => {
-      if (groupedTrainingSessions[key].length > 0) {
-        trainingDaysPerWeek++;
-      }
-    });
-    return trainingDaysPerWeek;
-  };
-
-  const groupedTrainingSessions = groupTrainingSessions(trainingSessions);
-  const trainingDaysPerWeek = calculateTrainingDaysPerWeek(
-    groupedTrainingSessions
-  );
 
   return (
     <Layout title="EvolveLogix | Training Log">
