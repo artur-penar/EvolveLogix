4cd38f3eea74f5a91ac64e70e1054e6bc840a109
Artur Penar <penar.art@gmail.com>
2024-10-21 06:12:33 +0200

    Frontend: Add index.js into trainingLogs handlers
    - Added index.js file with organized hanlders exports.
    - Imporved AddTrainingSession imports using index.js.

---
+++ frontend/src/features/trainingLogs/handlers/index.js
@@ -0,0 +1,8 @@
+export { default as handleAddExercise } from "./handleAddExercise";
+export { default as handleSetsNumberChange } from "./handleSetsNumberChange";
+export { default as handleExerciseDetailsChange } from "./handleExerciseDetailsChange";
+export { default as handleRemoveExercise } from "./handleRemoveExercise";
+export { default as handleWeightPercentageChange } from "./handleWeightPercentageChange";
+export { default as handleCheckboxChange } from "./handleCheckboxChange";
+export { default as handleExerciseChange } from "./handleExerciseChange";
+export { default as handleTrainingDataChange } from "./handleTrainingDataChange";
