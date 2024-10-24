821c60149278b79ff4d3ec5b33832a6297ce9abf
Artur Penar <penar.art@gmail.com>
2024-10-24 05:13:41 +0200

    Frontend: Add ExerciseDetilsSummaty component
    - Added component consist summary details to an exercise like Exercise volume, Average intensity(if avaiable) and total repetitions.

---
+++ frontend/src/features/trainingLogs/pages/AddTrainingSession/components/ExerciseDetailsSummary.js
@@ -0,0 +1,32 @@
+import calculateAverageIntensity from "features/trainingLogs/utils/calculateAverageIntensity";
+import calculateTotalRepetitions from "features/trainingLogs/utils/calculateTotalRepetitions";
+import calculateTotalVolume from "features/trainingLogs/utils/calculateTotalVolume";
+import React from "react";
+
+/**
+ * Component to display a summary of exercise details.
+ *
+ * @param {Object} props - The component props.
+ * @param {Object} props.exercise - The exercise data.
+ * @param {Object} props.strengthRecords - The strength records data.
+ *
+ * @returns {JSX.Element} A JSX element displaying exercise details summary.
+ */
+const ExerciseDetailsSummary = ({ exercise, strengthRecords }) => {
+  const volume = calculateTotalVolume(exercise);
+  const averageIntensity = calculateAverageIntensity({
+    exercise,
+    strengthRecord: strengthRecords[exercise.exercise],
+  });
+  return (
+    <div>
+      <p style={{ fontWeight: "bold" }}>Exercise Details</p>
+      <p>Exercise volume: {volume}</p>
+      {averageIntensity !== null && averageIntensity !== undefined && (
+        <p>Average intensity: {averageIntensity}%</p>
+      )}
+      <p>Total repetitions: {calculateTotalRepetitions(exercise)}</p>
+    </div>
+  );
+};
+export default ExerciseDetailsSummary;
