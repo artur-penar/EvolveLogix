6148247f415120683b30162559e4e492f94afb52
Artur Penar <penar.art@gmail.com>
2024-10-29 16:51:58 +0100

    Frontend: Update useDeleteModalState custom hook
    - Added docstring.

--- frontend/src/features/trainingLogs/pages/TrainingLogDashboard/hooks/useDeleteModalState.js
+++ frontend/src/features/trainingLogs/pages/TrainingLogDashboard/hooks/useDeleteModalState.js
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
 
+/**
+ * Custom hook that manages the state of a delete modal based on the delete message.
+ *
+ * @param {string} deleteMessage - The message that triggers the delete modal. If the message is not empty, the modal will be open.
+ * @param {Function} setIsDeleteModalOpen - Function to set the state of the delete modal. Should be a state setter function from useState.
+ */
 const useDeleteModalState = (deleteMessage, setIsDeleteModalOpen) => {
   useEffect(() => {
     setIsDeleteModalOpen(deleteMessage !== "");
