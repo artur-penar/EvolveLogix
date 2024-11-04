e562d3a9890397937a8229ffb4fa13960586dd10
Artur Penar <penar.art@gmail.com>
2024-11-04 14:40:13 +0100

    Frontend: Update LoadingState shared component
    - Updated inline styling to display components in the center of the screen.
    - Removed redundant 'Loading' paragraph.

--- frontend/src/components/shared/LoadingState.js
+++ frontend/src/components/shared/LoadingState.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const LoadingState = () => (
-  <div style={{ display: "flex", alignItems: "center" }}>
+  <div
+    style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+  >
     <div className="spinner-border" role="status">
-      <span className="sr-only">Loading...</span>
+      <span className="sr-only"></span>
     </div>
     <h2>Loading...</h2>
   </div>
