30db08950dcff03f8bfe73fdaff5186dcb0562a7
Artur Penar <penar.art@gmail.com>
2024-11-16 10:42:39 +0100

    Frotend: Update LogSelector.js and LogSelector.css file
    - Updated 'log-selector' stylign.
    - Provieded h5 header into LogSelector component.

--- frontend/src/features/users/components/LogSelector.js
+++ frontend/src/features/users/components/LogSelector.js
@@ -3,6 +3,7 @@ import "./LogSelector.css";
 
 const LogSelector = ({ trainingLogs, selectedLog, handleChange }) => (
   <div className="log-selector">
+  <h5>Selected Log:</h5>
     <select value={selectedLog.name} onChange={handleChange}>
       {trainingLogs.map((log) => (
         <option key={log.id} value={log.name}>
