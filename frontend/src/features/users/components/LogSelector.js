b87a1579ee8e41bc99fdf610f3fc3f0f9bcd63ee
Artur Penar <penar.art@gmail.com>
2024-11-19 15:13:04 +0100

    Frontend: Add TrainingLogPanel component
    - Grouped LogSelector and newLogForm components into the TrainingLogPanel parent component.
    - Added stylings in TrainingLogPanel.css.

--- frontend/src/features/users/components/LogSelector.js
+++ frontend/src/features/users/components/LogSelector.js
@@ -3,8 +3,8 @@ import "./LogSelector.css";
 
 const LogSelector = ({ trainingLogs, selectedLog, handleChange }) => (
   <div className="log-selector">
-  <h5>Selected Log:</h5>
-    <select value={selectedLog.name} onChange={handleChange}>
+    <h5>Select Log:</h5>
+    <select value={selectedLog.name} onChange={handleChange} size={3}>
       {trainingLogs.map((log) => (
         <option key={log.id} value={log.name}>
           {log.name}
