d0037bc916063f80ca4057d1509775d53d969776
Artur Penar <penar.art@gmail.com>
2024-12-01 07:55:09 +0100

    Frontend: Refactor NewLogForm component to use formData prop and add prop types validation

--- frontend/src/features/users/components/NewLogForm.js
+++ frontend/src/features/users/components/NewLogForm.js
@@ -1,18 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const NewLogForm = ({ newLogName, setNewLogName, handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <label>
-      Create new log:
-      <input
-        type="text"
-        placeholder="Enter log name"
-        value={newLogName}
-        onChange={(e) => setNewLogName(e.target.value)}
-      />
-    </label>
-    <button type="submit">Create log</button>
-  </form>
-);
+const NewLogForm = ({ formData }) => {
+  if (!formData) {
+    return null; // Handle the case where formData is undefined
+  }
+
+  return (
+    <form onSubmit={formData.handleSubmit}>
+      <label>
+        Create new log:
+        <input
+          type="text"
+          placeholder="Enter log name"
+          value={formData.newLogName}
+          onChange={(e) => formData.setNewLogName(e.target.value)}
+        />
+      </label>
+      <button type="submit">Create log</button>
+    </form>
+  );
+};
+
+NewLogForm.propTypes = {
+  formData: PropTypes.shape({
+    newLogName: PropTypes.string.isRequired,
+    setNewLogName: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+  }),
+};
 
 export default NewLogForm;
