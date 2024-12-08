2cb672fd98a430f79eefa3a9b2090bccd1bf2c1c
Artur Penar <penar.art@gmail.com>
2024-12-08 05:08:37 +0100

    Frontend: Enhance DetailDisplay component to support editable fields and save functionality

--- frontend/src/features/users/components/UserInfoPanel/DetailDisplay.js
+++ frontend/src/features/users/components/UserInfoPanel/DetailDisplay.js
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const DetailDisplay = ({ formData, handleEdit }) => (
-  <div className="body-measurements">
-    {Object.entries(formData).map(([key, value]) => (
-      <div
-        key={key}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: "10px",
-        }}
-      >
-        <label>{capitalizeFirstLetter(key)}:</label>
-        <input
-          className="centered-input"
-          type="number"
-          name={key}
-          value={value}
-          disabled
-          min="0"
-        />
-      </div>
-    ))}
-    <button className="user-details-button" onClick={handleEdit}>
-      Edit
-    </button>
-  </div>
-);
+const DetailDisplay = ({ formData, handleInputChange, handleSubmit }) => {
+  const [isEditable, setIsEditable] = useState(false);
+  return (
+    <div className="body-measurements">
+      {Object.entries(formData).map(([key, value]) => (
+        <div
+          key={key}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+          }}
+        >
+          <label>{capitalizeFirstLetter(key)}:</label>
+          <input
+            className="centered-input"
+            type="number"
+            name={key}
+            value={value}
+            disabled={!isEditable}
+            onChange={isEditable ? handleInputChange : null}
+            min="0"
+          />
+        </div>
+      ))}
+      {isEditable ? (
+        <button className="user-details-button" onClick={handleSubmit}>
+          Save
+        </button>
+      ) : (
+        <button
+          className="user-details-button"
+          onClick={() => setIsEditable(true)}
+        >
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default DetailDisplay;
