c5ed9890645b50b7910b4b15cf7a800db3acf4ba
Artur Penar <penar.art@gmail.com>
2024-12-15 06:01:18 +0100

    Frontend: Refactor useRecordIndices to add docstring for better documentation

--- frontend/src/features/users/components/Shared/StrengthRecords/useRecordIndices.js
+++ frontend/src/features/users/components/Shared/StrengthRecords/useRecordIndices.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
+/**
+ * Custom hook to manage indices of exercise records.
+ *
+ * @param {Object} exerciseRecords - An object where keys are exercise names and values are arrays of records.
+ * @returns {Object} - An object containing:
+ *   - {Object} initialRecordIndices: Initial indices of the last record for each exercise.
+ *   - {Object} currentRecordIndices: Current indices of the records for each exercise.
+ *   - {Function} handlePrev: Function to decrement the index of the specified exercise.
+ *   - {Function} handleNext: Function to increment the index of the specified exercise.
+ */
 const useRecordIndices = (exerciseRecords) => {
   const initialRecordIndices = Object.fromEntries(
     Object.entries(exerciseRecords).map(([key, data]) => [key, data.length - 1])
