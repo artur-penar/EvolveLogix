816fde0d93ed0a057d74da6baa6885557f7738ce
Artur Penar <penar.art@gmail.com>
2024-12-20 14:43:57 +0100

    Frontend: Add NewStrengthRecord component with styling for improved layout and user experience

--- frontend/src/features/users/components/StrengthRecordsPanel/NewStrengthRecord.js
+++ frontend/src/features/users/components/StrengthRecordsPanel/NewStrengthRecord.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectExerciseNames } from "features/trainingLogs/selectors";
 import { createStrengthRecord } from "features/users/strengthRecordSlice";
+import "./NewStrengthRecord.css";
 
 const NewStrengthRecord = () => {
   const dispatch = useDispatch();
@@ -36,71 +37,41 @@ const NewStrengthRecord = () => {
   };
 
   return (
-    <div
-      className="strength-records-container"
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <h4 className="header-container">Add new record</h4>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "40%",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-            marginBottom: "10px",
-          }}
-        >
-          <p>Exercise:</p>
+    <div className="new-strength-record-container">
+      <h4 className="header-container new-strength-record-header">
+        New Strength Record
+      </h4>
+      <div className="new-strength-record-form">
+        <div className="nsr-row">
+          <label htmlFor="exercise">Exercise</label>
           <select
-            className="form-control"
+            id="exercise"
+            name="exercise"
             value={exerciseName}
             onChange={handleExerciseChange}
-            style={{ width: "60%" }}
           >
-            {exercisesList.map((name, i) => (
-              <option key={i} value={name}>
-                {name}
+            {exercisesList.map((exercise) => (
+              <option key={exercise} value={exercise}>
+                {exercise}
               </option>
             ))}
           </select>
         </div>
-
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <p>Weight:</p>
+        <div className="nsr-row">
+          <label htmlFor="weight">Weight</label>
           <input
+            className="nsr-centered-input"
             type="number"
-            step="0.25"
-            min="0"
+            id="weight"
+            name="weight"
             value={weight}
             onChange={handleWeightChange}
-            style={{ width: "60%" }}
-          ></input>
+          />
+        </div>
+        <div className="nsr-row">
+          <button onClick={handleSubmit}>Submit</button>
         </div>
       </div>
-      <button
-        onClick={handleSubmit}
-        style={{
-          display: "block",
-          margin: "0 auto",
-          marginTop: "10px",
-          borderRadius: "10px",
-        }}
-      >
-        Submit
-      </button>
     </div>
   );
 };
