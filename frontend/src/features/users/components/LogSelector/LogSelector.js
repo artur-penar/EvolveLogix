81b59d9e22ca6216ba8d2c5c981b5841410a0dfa
Artur Penar <penar.art@gmail.com>
2025-01-07 10:01:12 +0100

    Frontend: Bold the "Select Log" header in LogSelector for improved emphasis

--- frontend/src/features/users/components/LogSelector/LogSelector.js
+++ frontend/src/features/users/components/LogSelector/LogSelector.js
@@ -3,7 +3,7 @@ import "./LogSelector.css";
 
 const LogSelector = ({ trainingLogs, selectedLog, handleChange }) => (
   <div className="log-selector">
-    <h5>Select Log:</h5>
+    <h5 style={{ fontWeight: "bold" }}>Select Log:</h5>
     <select value={selectedLog.name} onChange={handleChange} size={3}>
       {trainingLogs.map((log) => (
         <option key={log.id} value={log.name}>
