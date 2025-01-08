112bceb0bc87fead7f77eabed2bf5ff847eb5b0d
Artur Penar <penar.art@gmail.com>
2025-01-08 12:49:49 +0100

    Frontend: Add RegisterPage styles and refactor layout with new components

--- frontend/src/features/users/pages/RegisterPage/RegisterPage.js
+++ frontend/src/features/users/pages/RegisterPage/RegisterPage.js
@@ -3,6 +3,8 @@ import Layout from "shared/components/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { register } from "features/users/user";
+import Button from "@mui/material/Button";
+import "./RegisterPage.css";
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
@@ -31,49 +33,56 @@ const RegisterPage = () => {
 
   return (
     <Layout title="EvolveLogix| Register" content="Register page.">
-      <div className="dashboard">
-        <h1>Register</h1>
-        <form className="mt-5" onSubmit={onSubmit}>
-          <div className="form-group">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={onChange}
-              value={email}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label" htmlFor="name">
-              Name
-            </label>
-            <input
-              className="form-control"
-              type="text"
-              name="user_name"
-              onChange={onChange}
-              value={user_name}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="form-control"
-              type="password"
-              name="password"
-              onChange={onChange}
-              value={password}
-            />
-          </div>
-          <button className="btn btn-primary mt-4" type="submit">
-            Submit
-          </button>
-        </form>
+      <div className="register-page-container">
+        <div className="register-form-container">
+          <h1 style={{ marginTop: "1rem" }}>Register</h1>
+          <form className="mt-5" onSubmit={onSubmit}>
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                onChange={onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="user_name"
+                onChange={onChange}
+                value={user_name}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                onChange={onChange}
+                value={password}
+              />
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "1rem", marginBottom: "1rem" }}
+            >
+              Register
+            </Button>
+          </form>
+        </div>
       </div>
     </Layout>
   );
