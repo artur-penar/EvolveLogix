f7ae984fa4707a99d7a35208cd2df54a2ddffeaf
Artur Penar <penar.art@gmail.com>
2025-01-08 06:21:26 +0100

    Frontend: Refactor LoginPage to use MUI components and improve layout with additional links
    - Added MUI Login button.
    - Added the 'Forgot Password' and 'Sign Up' MUI links.

--- frontend/src/features/users/pages/LoginPage/LoginPage.js
+++ frontend/src/features/users/pages/LoginPage/LoginPage.js
@@ -2,12 +2,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Layout from "shared/components/Layout";
+import Header from "shared/components/PageHeader";
 import Button from "@mui/material/Button";
 import {
   login,
   resetRegistered,
   selectIsUserAuthenticated,
 } from "features/users/user";
+
+import "./LoginPage.css";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Link } from "@mui/material";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -39,37 +50,61 @@ const LoginPage = () => {
 
   return (
     <Layout title="EvolveLogix | Login" content="Login page.">
-      <div className="dashboard">
-        <h1>Log into your account</h1>
-        <form className="mt-5" onSubmit={onSubmit}>
-          <div className="form-group">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={onChange}
-              value={email}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="form-control"
-              type="password"
-              name="password"
-              onChange={onChange}
-              value={password}
-            />
-          </div>
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </form>
+      <div className="login-page-container">
+        {/* <Header headerContent={"Login"} /> */}
+
+        <div className="login-form-container">
+          <h1 style={{ marginTop: "1rem" }}>Login</h1>
+          <form className="mt-5" onSubmit={onSubmit}>
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                onChange={onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                onChange={onChange}
+                value={password}
+              />
+            </div>
+            <Link href="#" variant="body1">
+              Forgot Password?
+            </Link>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "1rem" }}
+            >
+              Login
+            </Button>
+            <span
+              style={{
+                marginTop: "1rem",
+                marginBottom: "1rem",
+                display: "block",
+              }}
+            >
+              Don't have an account?{" "}
+              <Link href="/register" variant="body1">
+                Sign up
+              </Link>
+            </span>
+          </form>
+        </div>
       </div>
     </Layout>
   );
